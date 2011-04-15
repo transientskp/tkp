e721d8897fb4a4d7667044d3f70e579145d4f6bf
Evert Rol <evert.astro@gmail.com>
2011-04-15 14:52:17 +0000

    Removed database module from classification package
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@1807 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- tkp/classification/database/create.table.classification.sql
+++
@@ -1,8 +0,0 @@
-/**
- * This table contains classification of transients
- */
-CREATE TABLE "classification" (
-	"transient_id"   int           NOT NULL,
-	"classification" varchar(256),
-	"weight"         double        NOT NULL        DEFAULT 0
-);

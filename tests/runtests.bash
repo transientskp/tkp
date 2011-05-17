5a77d1f05ef69939b40a8668ef75f02102f0a95d
Evert Rol <evert.astro@gmail.com>
2011-05-17 15:52:44 +0000

    Adjusted PYTHONPATHs in trunk/runtests.bash for CEP1
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@1876 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- tests/runtests.bash
+++ tests/runtests.bash
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 export PYTHONPATH=./tkp/python-packages/lib.linux-x86_64-2.6:./tkp/trunk/external/deconv:./tkp/trunk/external/python-wcslib
+# Extra paths needed on CEP1
+export PYTHONPATH=./tkp/python-packages/lib:${PYTHONPATH}:/opt/pythonlibs/lib/python2.5/site-packages/:/opt/MonetDB/lib/python2.5/site-packages/
 export LD_LIBRARY_PATH=./tkp/trunk/external/libwcstools
 dir=`dirname $0`
 echo "Running python TKP tests"

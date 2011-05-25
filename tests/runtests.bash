6ae17091e76f2954d410b72f2b5715c2ab4f3205
Evert Rol <evert.astro@gmail.com>
2011-05-25 08:30:12 +0000

    Adjusted runtests script to account for PYTHON/LD_LIBRARY-PATH settings
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@1907 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- tests/runtests.bash
+++ tests/runtests.bash
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-export PYTHONPATH=./tkp/python-packages/lib.linux-x86_64-2.6:./tkp/trunk/external/deconv:./tkp/trunk/external/python-wcslib
-# Extra paths needed on CEP1
-export PYTHONPATH=./tkp/python-packages/lib:${PYTHONPATH}:/opt/pythonlibs/lib/python2.5/site-packages/:/opt/MonetDB/lib/python2.5/site-packages/
-export LD_LIBRARY_PATH=./tkp/trunk/external/libwcstools
+if [ -z "$PYTHONPATH" ]
+then
+    export PYTHONPATH=./tkp/python-packages/lib.linux-x86_64-2.6:./tkp/trunk/external/deconv:./tkp/trunk/external/python-wcslib
+else
+    export PYTHONPATH=${PYTHONPATH}:./tkp/python-packages/lib.linux-x86_64-2.6:./tkp/trunk/external/deconv:./tkp/trunk/external/python-wcslib
+fi
+if [ -z "$LD_LIBRARY_PATH" ]
+then
+    export LD_LIBRARY_PATH=./tkp/trunk/external/libwcstools
+else
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:./tkp/trunk/external/libwcstools
+fi
+
 dir=`dirname $0`
 echo "Running python TKP tests"
 python ${dir}/test.py

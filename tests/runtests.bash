15efc9cdbdec4453a771c9448f0668619716321a
Evert Rol <evert.astro@gmail.com>
2011-06-01 13:40:02 +0000

    Changed runtests.bash to also run on OS X
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@1935 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- tests/runtests.bash
+++ tests/runtests.bash
@@ -11,8 +11,18 @@ ld_library_path=./tkp/trunk/external/libwcstools
 if [ -z "$LD_LIBRARY_PATH" ]
 then
     export LD_LIBRARY_PATH=${ld_library_path}
+    export DYLD_LIBRARY_PATH=${ld_library_path}
 else
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ld_library_path}
+    export DYLD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ld_library_path}
+fi
+# Needs check whether we're on OS X, but setting an extra
+# environment variable should not be a problem
+if [ -z "$DYLD_LIBRARY_PATH" ]
+then
+    export DYLD_LIBRARY_PATH=${ld_library_path}
+else
+    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${ld_library_path}
 fi
 
 dir=`dirname $0`

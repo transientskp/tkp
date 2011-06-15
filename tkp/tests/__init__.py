095e595c60dc7ec5782e950bf1e6e93aa97fd1a0
Evert Rol <evert.astro@gmail.com>
2011-06-15 13:15:03 +0000

    Updated TKP list of tests
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@1972 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- tkp/tests/__init__.py
+++ tkp/tests/__init__.py
@@ -2,18 +2,17 @@ import unittest
 
 testfiles = [
     'tkp.tests.accessors',
-    'tkp.tests.database',
-    'tkp.tests.dataset',
     'tkp.tests.classification',
     'tkp.tests.config',
     'tkp.tests.coordinates',
+    'tkp.tests.database',
+    'tkp.tests.dataset',
     'tkp.tests.FDR',
     'tkp.tests.feature_extraction',
     'tkp.tests.gaussian',
     'tkp.tests.L15_12h_const',
     'tkp.tests.sigmaclip',
     'tkp.tests.source_measurements',
-    'tkp.tests.utilities',
     'tkp.tests.wcs',
 ]
 

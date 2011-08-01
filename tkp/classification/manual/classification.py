cb7ac04a01ce40e8e1848657d9c39ef9eb674c02
Evert Rol <evert.astro@gmail.com>
2011-08-01 12:26:28 +0000

    Updated manual classification examples and tests
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@2072 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- tkp/classification/manual/classification.py
+++ tkp/classification/manual/classification.py
@@ -6,22 +6,36 @@ from .classifier import ClassifiedTransient, Branch
 This file defines the various transients that can be classified. It
 does so by creating classes that inherit from ClassifiedTransient, and
 that define test methods. Each test method can check for one or more
-Transient attributes, and returns a weight depending on the outcome
-(there is no need to return a default value of 0.0: just let the
-method return automatically return None if the checks don't pass.
-
-As an extra option, branches can be introduced, which can bypass
-entire classification tests. These are created by defining a subclass
-of Branch, and creating test methods. Each test method should return a
-list, either empty or consisting of classes; those classes are, again,
-subclasses of ClassifiedTransients or Branch, and will be run next
-when the code is executed. Branches can thus be nested.
+Transient attributes, and returns a weight depending on the test
+outcome (there is no need to return a default value of 0.0: just let
+the method return automatically if the checks don't pass; the
+implicitly returned 'None' will be regarded as 0.0).
 
 The test method names, for both ClassifiedTransient and Branch
 subclasses, should start with 'test', extended by any legimate
 character combination (much like the unittest framework). The test
 method are not guaranteed to be run in order, and should thus not be
-dependent on each other.
+dependent on each other.  When multiple test are executed, the various
+return values (weights) are added together, to result in a single
+weight for the specific classified transient.
+
+To start the classification tests for a transient, call the classify()
+method (defined in ClassifiedTransient). It will run the various
+tests, and return a single-item list with a tuple containing the doc
+string of the ClassifiedTransient subclass, and the weight as the
+second tuple item (an example returned value could be
+[("some transient", 0.6)]). The results are returned as a list because
+other classify() methods will be able to extend that list: a single
+transient can thus have multiple classification.
+
+As an extra option, branches can be introduced, which can bypass
+entire classification tests. These are created by defining a subclass
+of Branch, and creating test methods. Each test method should return a
+list, either empty or consisting of classes; those classes are, again,
+subclasses of ClassifiedTransients or Branch, and will be called when
+the code is executed; branches can thus be nested. The returned value
+will, in the end, be a list of 2-tuples with a description string and
+a weight for each ClassifiedTransient.
 
 """
 
@@ -36,6 +50,19 @@ class SlowTransient(ClassifiedTransient):
 
     def test_variability(self):
         if self.variability > 1e4:
+            return 0.9
+
+
+class FastTransient(ClassifiedTransient):
+
+    """Fast transient"""
+
+    def test_duration(self):
+        if self.duration < 1e3:
+            return 0.9
+
+    def test_variability(self):
+        if self.variability < 1e2:
             return 0.9
 
 
@@ -55,22 +82,39 @@ class GRBPrompt(ClassifiedTransient):
         if 0 < self.variability < 1e3:
             return 0.6
 
+class DatabaseSource(ClassifiedTransient):
+
+    """Any transient that can be associated with a database"""
+
+    def test_database(self):
+        if len(self.database):
+            return 1.0
+    
 
 class MainBranch(Branch):
 
     """Starting point for manual classification tree"""
     
     def test1(self):
-        return [SubBranch1]
+        return [SubBranch1a, SubBranch1b]
 
     def test2(self):
         return [SubBranch2]
 
+    def test3(self):
+        return [DatabaseSource]
+    
 
-class SubBranch1(Branch):
+class SubBranch1a(Branch):
 
     def test1(self):
         return [SlowTransient]
+
+
+class SubBranch1b(Branch):
+
+    def test1(self):
+        return [FastTransient]
 
 
 class SubBranch2(Branch):
@@ -81,7 +125,7 @@ class SubBranch2(Branch):
 
 class SpectralBranch(Branch):
 
-    """Spectral branch"""
+    """Negative spectral index"""
 
     def test1(self):
         if self.transient.spectralindex < 0:
--- tkp/tests/classification.py
+++ tkp/tests/classification.py
@@ -9,6 +9,9 @@ from tkp.classification.manual.classification import MainBranch
 from tkp.classification.manual.classification import SpectralBranch
 from tkp.classification.manual.classifier import Classifier
 from tkp.classification.manual.transient import Transient
+from tkp.classification.manual.transient import Int
+from tkp.classification.manual.transient import Float
+from tkp.classification.manual.transient import String
 
 
 
@@ -21,16 +24,34 @@ class TestManual(unittest.TestCase):
         self.transient2 = Transient(6, 1e7, 1e6, spectralindex=-1)
 
     def test_grb_prompt(self):
+        """Only check whether this is a GRB prompt event"""
+        
         classifier = Classifier(self.transient1, GRBPrompt)
         self.assertEqual(classifier.classify(), [('GRB prompt emission', 1.2)])
+        classifier = Classifier(self.transient2, GRBPrompt)
+        self.assertEqual(classifier.classify(), [('GRB prompt emission', 0.)])
 
+    def test_spectrum(self):
+        """Test whether this source has a negative index.
+        If so, proceed with next tests.
+        """
+
+        classifier = Classifier(self.transient1, SpectralBranch)
+        self.assertEqual(classifier.classify(), [])
+        classifier = Classifier(self.transient2, SpectralBranch)
+        self.assertEqual(classifier.classify(), [('Slow transient', 1.8), ('GRB prompt emission', 0.)])
+        
     def test_all(self):
+        """Test for all defined classifications"""
+        
         classifier = Classifier(self.transient1, MainBranch)
         self.assertEqual(classifier.classify(),
-                    [('Slow transient', 0), ('GRB prompt emission', 1.2)])
+                    [('Slow transient', 0.), ('Fast transient', 1.8), ('GRB prompt emission', 1.2),
+                     ('Any transient that can be associated with a database', 1.0)])
         classifier = Classifier(self.transient2, MainBranch)
         self.assertEqual(classifier.classify(),
-                    [('Slow transient', 1.8), ('GRB prompt emission', 0)])
+                    [('Slow transient', 1.8), ('Fast transient', 0.), ('GRB prompt emission', 0),
+                     ('Any transient that can be associated with a database', 0.)])
 
     def test_branching(self):
         self.assertEqual(SpectralBranch(self.transient2).classify(),

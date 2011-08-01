ab0b5a9ed897035472046bd0bc66a0d2563a2d72
Evert Rol <evert.astro@gmail.com>
2011-08-01 12:26:31 +0000

    Reversed previous hacks around the non-existent transient features; now solved using an Undefined class.
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@2076 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- tkp/classification/manual/transient.py
+++ tkp/classification/manual/transient.py
@@ -12,12 +12,53 @@ A Transient object class, that stores a variety of information related to any ty
 
 from .utils import Position
 from .utils import DateTime
-from .utils import Int
-from .utils import Float
-from .utils import String
 
 
-        
+
+class Undefined(object):
+    """This is a small helper class to use for undefined values.
+
+    Python's None evalutes to True or False in comparisons with actual
+    values, instead of raising a TypeError. This prevents it from
+    being used in the classification.py user interface, because that
+    would horribly complicate the tests. Instead, if a value is simply
+    unavailable, it will be assigned an Undefined() instance, and thus
+    raise a TypeError.
+
+    Note that this problem is particular to Python 2; Python 3 does
+    the proper thing:
+    http://docs.python.org/release/3.0.1/whatsnew/3.0.html#ordering-comparisons
+
+    """
+    
+    # Note: special methods are bypassed by the Python interprester,
+    # so there can't be a catch-all way of using eg __getattribute__
+    # (http://docs.python.org/reference/datamodel.html#\
+    # special-method-lookup-for-old-style-classes) We'll have to
+    # define all those we want to catch
+    
+    def __lt__(self, other):
+        raise TypeError("Cannot compare an undefined value")
+    
+    def __gt__(self, other):
+        raise TypeError("Cannot compare an undefined value")
+    
+    def __le__(self, other):
+        raise TypeError("Cannot compare an undefined value")
+    
+    def __ge__(self, other):
+        raise TypeError("Cannot compare an undefined value")
+    
+    def __eq__(self, other):
+        raise TypeError("Cannot compare an undefined value")
+    
+    def __ne__(self, other):
+        raise TypeError("Cannot compare an undefined value")
+    
+    def __contains__(self, other):
+        raise TypeError("Cannot compare an undefined value")
+
+
 class Transient(object):
 
     """The Transient class defines a transient object
@@ -71,42 +112,41 @@ class Transient(object):
 
         """
         self.srcid = int(srcid)
-        self.duration = Float(duration)
-        self.variability = Float(variability)
+        self.duration = Undefined() if duration is None else duration
+        self.variability = Undefined() if variability is None else variability
         self.database = database if database else []
-        self.shape = String(shape)
-        self.position = position
-        self.timezero = timezero
-        self.spectralindex = Float(spectralindex)
-        if features is None:
-            features = {}
+        self.shape = Undefined() if shape is None else shape
+        self.position = Undefined() if position is None else position
+        self.timezero = Undefined() if timezero is None else timezero
+        self.spectralindex = Undefined() if spectralindex is None else spectralindex
+        features = {} if features is None else features
 
     def __str__(self):
-        if self.duration is not None:
-            if self.variability is not None:
+        if hasattr(self, 'duration'):
+            if hasattr(self, 'variability'):
                 return "transient with duration %-6g and variability %-6g" % (
                     self.duration, self.variability)
             else:
                 return "transient with duration %-6g" % (self.duration)
-        elif self.variability is not None:
+        elif hasattr(self, 'variability'):
             return "transient with variability %-6g" % (self.variability)
         else:
             return "transient source"
 
     def __repr__(self):
         arglist = ["srcid=%d" % self.srcid]
-        if not self.duration.isNone:
+        if not isinstance(self.duration, Undefined):
             arglist.append("duration=%.1f" % self.duration)
-        if not self.variability.isNone:
+        if not isinstance(self.variability, Undefined):
             arglist.append("variability=%.2f" % self.variability)
-        if len(self.database):
+        if len(database):
             arglist.append("database=%s" % self.database)
-        if self.position is not None:
+        if not isinstance(self.position, Undefined):
             arglist.append("position=%s" % repr(self.position))
-        if self.timezero is not None:
+        if not isinstance(self.timezero, Undefined):
             arglist.append("timezero=%s" % repr(self.timezero))
-        if not self.shape.isNone:
+        if not isinstance(self.shape, Undefined):
             arglist.append("shape=%s" % self.shape)
-        if not self.spectralindex.isNone:
+        if not isinstance(self.spectralindex, Undefined):
             arglist.append("spectralindex=%.1f" % self.spectralindex)
         return "%s(%s)" % (self.__class__.__name__, ", ".join(arglist),)

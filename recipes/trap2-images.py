9d333a69313430511b3a9860d14a1422eed34703
Evert Rol <evert.astro@gmail.com>
2011-08-03 10:00:27 +0000

    Added images-only TRAP recipe
    git-svn-id: http://svn.transientskp.org/code/trap/trunk@2084 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

---
+++ recipes/trap2-images.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+"""
+This main recipe accepts a list of images (through images_to_process.py).
+Images should be prepared with the correct keywords.
+"""
+
+from __future__ import with_statement
+
+import sys
+import os
+from contextlib import closing
+from operator import itemgetter
+from itertools import repeat
+
+from pyrap.quanta import quantity
+
+from lofarpipe.support.control import control
+from lofarpipe.support.utilities import log_time
+from lofarpipe.support.parset import patched_parset
+import tkp.database.database as tkpdb
+
+class SIP(control):
+    def pipeline_logic(self):
+        from images_to_process import images
+        dataset_id = None
+        with log_time(self.logger):
+             for image in images:
+                    outputs = self.run_task("source_extraction2",
+                                            images=[image],
+                                            dataset_id=dataset_id)
+                    if dataset_id is None:
+                        dataset_id = outputs['dataset_id']
+                    dblogin = dict([(key, self.config.get('database', key))
+                                for key in ('name', 'user', 'password',
+                                            'host')])
+
+                    with closing(tkpdb.DataBase(**dblogin)) as database:
+                        self.logger.info("%s", str(database))
+
+                        outputs.update(
+                            self.run_task("transient_search2", [],
+                                          dataset_id=dataset_id,
+                                          database=database)
+                            )
+                    outputs.update(
+                        self.run_task("feature_extraction2", [],
+                                      transients=outputs['transients'],
+                                      dblogin=dblogin,  # for the compute nodes
+                                      database=database)
+                        )
+
+                    # run the manual classification on the transient objects
+                    outputs.update(
+                        self.run_task("classification", [],
+                                      transients=outputs['transients'],
+                                      database=database)
+                        )
+
+                    self.run_task("prettyprint2", [], transients=outputs['transients'])
+
+if __name__ == '__main__':
+    sys.exit(SIP().main())

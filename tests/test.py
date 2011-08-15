8758052680442170a9e11643b7ebdc89e93702ff
John Swinbank <swinbank@transientskp.org>
2011-08-15 16:35:03 +0000

    Print a warning if a test suite fails to run due to an import error
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@2122 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- tests/test.py
+++ tests/test.py
@@ -13,16 +13,20 @@ import tkp.tests
 # due to test failures rather than log messages.
 logging.basicConfig(level=logging.CRITICAL)
 
-# Check that each of our tests can be imported
-# Otherwise, unittest throws confusing errors
-for test in tkp.tests.testfiles:
-    __import__(test)
-
 args = ['tkp.tests.' + arg for arg in sys.argv[1:]]
 if args:
     testfiles = [testfile for testfile in tkp.tests.testfiles if testfile in args]
 else:
     testfiles = tkp.tests.testfiles
+
+# If we can't import a particular module, we should warn the user and move on
+# with the test suite, rather than bailing out completely.
+for test in testfiles:
+    try:
+        __import__(test)
+    except ImportError, e:
+        print >>sys.stderr, "WARNING: Not running %s: required module failed to import (%s)" % (test, e)
+        testfiles.remove(test)
 
 result = unittest.TextTestRunner(verbosity=2).run(
     unittest.TestLoader().loadTestsFromNames(testfiles)

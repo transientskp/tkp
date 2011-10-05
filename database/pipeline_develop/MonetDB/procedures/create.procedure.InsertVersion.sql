269f1447994decd6ef6f002543a7a78546c8f072
Bart Scheers <L.H.A.Scheers@uva.nl>
2011-10-05 07:30:49 +0000

    report on db version in versions table
    git-svn-id: http://svn.transientskp.org/code/database/trunk@2236 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- database/pipeline_develop/MonetDB/procedures/create.procedure.InsertVersion.sql
+++ database/pipeline_develop/MonetDB/procedures/create.procedure.InsertVersion.sql
@@ -8,11 +8,13 @@ BEGIN
   INSERT INTO versions
     (version
     ,creation_date
+    ,monet_version
     ,scriptname
     ) 
   VALUES
     ('0.0.1'
     ,(SELECT now())
+    ,(SELECT value FROM sys.env() WHERE name = 'monet_version')
     ,'/pipe/database/pipeline_develop/MonetDB/'
     )
   ;
--- database/pipeline_develop/MonetDB/tables/create.table.versions.sql
+++ database/pipeline_develop/MonetDB/tables/create.table.versions.sql
@@ -3,11 +3,12 @@
  */
 CREATE SEQUENCE "seq_versions" AS INTEGER;
 
-CREATE TABLE versions (
-  versionid INT DEFAULT NEXT VALUE FOR "seq_versions",
-  version VARCHAR(32) NULL,
-  creation_date DATE NOT NULL,
-  scriptname VARCHAR(256) NULL,
-  PRIMARY KEY (versionid)
+CREATE TABLE versions 
+  (versionid INT DEFAULT NEXT VALUE FOR "seq_versions"
+  ,version VARCHAR(32) NULL
+  ,creation_date DATE NOT NULL
+  ,monet_version VARCHAR(8) NOT NULL
+  ,scriptname VARCHAR(256) NULL
+  ,PRIMARY KEY (versionid)
 );
 

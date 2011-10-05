e5bfe54443d79349e740a0c03f840998dd6cbe87
Bart Scheers <L.H.A.Scheers@uva.nl>
2011-10-05 07:57:07 +0000

    cross-catalogue matching per zone added
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@2238 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- scripts/sip_cross_assoc_cats.py
+++ scripts/sip_cross_assoc_cats.py
@@ -8,24 +8,24 @@ import tkp.database.siputils as sip
 import monetdb.sql as db
 from monetdb.sql import Error as Error
 
-host = sys.argv[1] # db host name
+#host = sys.argv[1] # db host name
 
 db_type = "MonetDB"
-db_host = host
-db_port = 50000
-db_dbase = "sip"
-db_user = "sip"
-db_passwd = "sip"
+db_host = "heastro1"
+db_port = 60000
+db_dbase = "sipgsm"
+db_user = "sipgsm"
+db_passwd = "sipgsm"
 
-conn = db.connect(hostname=db_host,database=db_dbase,username=db_user,password=db_passwd)
+conn = db.connect(hostname=db_host,port=db_port,database=db_dbase,username=db_user,password=db_passwd)
 
 starttime = time.time()
-basedir = '/home/scheers/log/tkp'
+basedir = os.getenv("HOME") + '/log/tkp'
 
 print "\nResults will be stored in", db_type, "dbname:", db_dbase
 
 logtime = time.strftime("%Y%m%d-%H%M")
-logfile = basedir + 'cross_assoc_cats_' + db_type + '_' + db_dbase + '_' + logtime + '.log'
+logfile = basedir + '/cross_assoc_cats_' + db_type + '_' + db_dbase + '_' + logtime + '.log'
 log = open(logfile, 'w')
 #log.write('-')
 
@@ -42,25 +42,21 @@ try:
     """
     #deRuiter_r = 6./3600.
     
-    #ra_min = 20.
-    #ra_max = 21.
-    #decl_min = 30.
-    #decl_max = 31.
-    #ra_min = 0.
-    #ra_max = 360.
-    #decl_min = 30.
-    #decl_max = 70.
-    ra_min = 284.
-    ra_max = 292.
-    decl_min = 1.
-    decl_max = 9.
+    #ra_min = 284.
+    #ra_max = 292.
+    #decl_min = 1.
+    #decl_max = 9.
+    ra_min = 0.
+    ra_max = 360.
+    decl_min = 44.
+    decl_max = 64.
     #c = [4, 5, 6, 3] # You have to know the ids ;) VLSS, WENSS main, WENSS polar, NVSS, resp.
     c = [4, 5, 6, 3] 
-    #sip.cross_associate_cataloged_sources(conn, c, ra_min, ra_max, decl_min, decl_max)
+    sip.cross_associate_cataloged_sources(conn, c, ra_min, ra_max, decl_min, decl_max)
     #dbu.variability_detection(conn, dataset.id, 0.2, 3.0)
     t_proc = time.time() - t_start
     print "processing: t =", str(t_proc), "s"
-    sip.get_merged_catalogs(conn, 288., 5., 3600.)
+    #sip.get_merged_catalogs(conn, 288., 5., 3600.)
     log.write(str(t_proc) + "\n") 
 
     conn.close()

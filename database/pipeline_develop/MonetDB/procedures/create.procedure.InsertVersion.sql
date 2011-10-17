8e5ea44d40376caff1f64063e6b9b029fdea3d7a
Bart Scheers <L.H.A.Scheers@uva.nl>
2011-10-17 13:53:18 +0000

    more db info in table
    git-svn-id: http://svn.transientskp.org/code/database/trunk@2258 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- database/pipeline_develop/MonetDB/procedures/create.procedure.InsertVersion.sql
+++ database/pipeline_develop/MonetDB/procedures/create.procedure.InsertVersion.sql
@@ -7,14 +7,16 @@ BEGIN
 
   INSERT INTO versions
     (version
-    ,creation_date
+    ,creation_ts
     ,monet_version
+    ,monet_release
     ,scriptname
     ) 
   VALUES
     ('0.0.1'
-    ,(SELECT now())
+    ,(SELECT CAST(NOW() AS TIMESTAMP))
     ,(SELECT value FROM sys.env() WHERE name = 'monet_version')
+    ,(SELECT value FROM sys.env() WHERE name = 'monet_release')
     ,'/pipe/database/pipeline_develop/MonetDB/'
     )
   ;
--- database/pipeline_develop/MonetDB/tables/create.table.versions.sql
+++ database/pipeline_develop/MonetDB/tables/create.table.versions.sql
@@ -6,8 +6,9 @@ CREATE SEQUENCE "seq_versions" AS INTEGER;
 CREATE TABLE versions 
   (versionid INT DEFAULT NEXT VALUE FOR "seq_versions"
   ,version VARCHAR(32) NULL
-  ,creation_date DATE NOT NULL
+  ,creation_ts TIMESTAMP NOT NULL
   ,monet_version VARCHAR(8) NOT NULL
+  ,monet_release VARCHAR(32) NOT NULL
   ,scriptname VARCHAR(256) NULL
   ,PRIMARY KEY (versionid)
 );

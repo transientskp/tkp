16d9f558c99279074f3ee13a97f4e6390caa3d20
Evert Rol <evert.astro@gmail.com>
2011-10-20 13:56:55 +0000

    Added catalog matching as classification feature
    git-svn-id: http://svn.transientskp.org/code/tkp/trunk@2266 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

---
+++ tkp/classification/features/catalogs.py
@@ -0,0 +1,37 @@
+"""
+
+Module that checks the database for source associations
+
+
+If the database is not available or the database module cannot be
+imported, functions will silently return None.
+"""
+
+
+import_failed = False
+try:
+    from tkp.database.database import DataBase
+    from tkp.database.utils import match_nearest_in_catalogs
+    
+except ImportError:
+    import_failed = True
+
+def match_catalogs(transient):
+    if import_failed:
+        return None
+    # Hardcode the catalogs for now
+    catalogs = {3: 'NVSS', 4: 'VLSS', 5: 'WENSS', 6: 'WENSS'} 
+    database = DataBase()
+    pos = transient.position
+    results = {}
+    for key, value in catalogs.iteritems():
+        results[value] = match_nearest_in_catalogs(
+            database.connection, pos.ra, pos.dec, pos.ra_err, pos.dec_err,
+            radius=1, catalogid=key, assoc_r=.1)
+        if len(results[value]) > 0:
+            results[value] = results[value][0]
+        else:
+            results[value] = {}
+    return results
+
+            

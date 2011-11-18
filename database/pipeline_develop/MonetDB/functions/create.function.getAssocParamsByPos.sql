d4de04dd722a1281d9160014fd1b293b4aad3aa6
Bart Scheers <L.H.A.Scheers@uva.nl>
2011-11-18 11:12:15 +0000

    db function returning assoc params of two sources
    git-svn-id: http://svn.transientskp.org/code/database/trunk@2357 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- database/pipeline_develop/MonetDB/functions/create.function.getAssocParamsByPos.sql
+++ database/pipeline_develop/MonetDB/functions/create.function.getAssocParamsByPos.sql
@@ -1,4 +1,6 @@
-create function getAssocParamsByPos(ira1 DOUBLE
+--DROP FUNCTION getAssocParamsByPos;
+
+CREATE FUNCTION getAssocParamsByPos(ira1 DOUBLE
                                    ,idecl1 DOUBLE
                                    ,ira_err1 DOUBLE
                                    ,idecl_err1 DOUBLE
@@ -11,7 +13,7 @@ create function getAssocParamsByPos(ira1 DOUBLE
                                                    ,assoc_log_lr DOUBLE
                                                    )
 
-begin
+BEGIN
 
   DECLARE x1, y1, z1, x2, y2, z2, ixy DOUBLE;
   
@@ -25,28 +27,32 @@ begin
   SET y2 = ixy * SIN(RADIANS(ira2));
   SET z2 = SIN(RADIANS(idecl2));
 
-  return table (
+  RETURN TABLE (
     select 3600 * DEGREES(2 * ASIN(SQRT( (x1 - x2) * (x1 - x2)
-                                   + (y1 - y2) * (y1 - y2)
-                                   + (z1 - z2) * (z1 - z2)
-                                   )
-                               / 2
-                              )
-                     ) AS assoc_distance_arcsec
-          ,SQRT( (ira1 * COS(RADIANS(idecl1)) - ira2 * COS(RADIANS(idecl2))) * (ira1 * COS(RADIANS(idecl1)) - ira2 * COS(RADIANS(idecl2)))
-                 / (ira_err1 * ira_err1 + ira_err2 * ira_err2)
-               + (idecl1 - idecl2) * (idecl1 - idecl2)  
-                 / (idecl_err1 * idecl_err1 + idecl_err2 * idecl_err2)
-               ) AS assoc_r
-          ,LOG10(EXP((( (ira1 * COS(RADIANS(idecl1)) - ira2 * COS(RADIANS(idecl2))) * (ira1 * COS(RADIANS(idecl1)) - ira2 * COS(RADIANS(idecl2))) 
-                        / (ira_err1 * ira_err1 + ira_err2 * ira_err2)
-                       + (idecl1 - idecl2) * (idecl1 - idecl2)  
-                        / (idecl_err1 * idecl_err1 + idecl_err2 * idecl_err2)
+                                       + (y1 - y2) * (y1 - y2)
+                                       + (z1 - z2) * (z1 - z2)
+                                       )
+                                  / 2
+                                  )
+                         ) AS assoc_distance_arcsec
+          ,3600 * SQRT(  (ira1 * COS(RADIANS(idecl1)) - ira2 * COS(RADIANS(idecl2))) 
+                       * (ira1 * COS(RADIANS(idecl1)) - ira2 * COS(RADIANS(idecl2)))
+                       / (ira_err1 * ira_err1 + ira_err2 * ira_err2)
+                      + (idecl1 - idecl2) * (idecl1 - idecl2)  
+                       / (idecl_err1 * idecl_err1 + idecl_err2 * idecl_err2)
+                      ) AS assoc_r
+          ,LOG10(EXP(((  (ira1 * COS(RADIANS(idecl1)) - ira2 * COS(RADIANS(idecl2))) 
+                       * (ira1 * COS(RADIANS(idecl1)) - ira2 * COS(RADIANS(idecl2))) 
+                       / (ira_err1 * ira_err1 + ira_err2 * ira_err2)
+                      + (idecl1 - idecl2) * (idecl1 - idecl2)  
+                       / (idecl_err1 * idecl_err1 + idecl_err2 * idecl_err2)
                       )
                      ) / 2
                     )
                  /
-                 (2 * PI() * SQRT(ira_err1 * ira_err1 + ira_err2 * ira_err2) * SQRT(idecl_err1 * idecl_err1 + idecl_err2 * idecl_err2) * 4.02439375E-06)
+                 (2 * PI() * SQRT(ira_err1 * ira_err1 + ira_err2 * ira_err2)    
+                           * SQRT(idecl_err1 * idecl_err1 + idecl_err2 * idecl_err2) * 4.02439375E-06
+                 )
                 ) AS assoc_log_lr
   )
   ;

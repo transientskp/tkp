924cc08204bf2c11c736e2123c651a8ab10d3c65
Evert Rol <evert.astro@gmail.com>
2011-11-24 11:13:18 +0000

    Added base recipe that includes email alerts
    git-svn-id: http://svn.transientskp.org/code/trap/trunk@2411 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

---
+++ recipes/trap-alerts.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+"""
+This main recipe accepts a list of images (through images_to_process.py).
+Images should be prepared with the correct keywords.
+"""
+
+from __future__ import with_statement
+
+import sys
+import os
+import datetime
+
+from pyrap.quanta import quantity
+
+from lofarpipe.support.control import control
+from lofarpipe.support.utilities import log_time
+from lofarpipe.support.parset import patched_parset
+
+from tkp.database.database import DataBase
+from tkp.database.dataset import DataSet
+
+
+class SIP(control):
+    def pipeline_logic(self):
+        from images_to_process import images
+
+        # Create the dataset
+        database = DataBase()
+        dataset = DataSet(data={'dsinname': self.inputs['job_name']},
+                          database=database)
+        with log_time(self.logger):
+             for image in images:
+                outputs = self.run_task("source_extraction",
+                                        images=[image],
+                                        dataset_id=dataset.id)
+                outputs.update(
+                    self.run_task("transient_search", [],
+                                  dataset_id=dataset.id))
+                outputs.update(
+                    self.run_task("monitoringlist",
+                                  image_ids=outputs['image_ids']))
+                outputs.update(
+                    self.run_task("feature_extraction", [],
+                                  transients=outputs['transients']))
+
+                # run the manual classification on the transient objects
+                outputs.update(
+                    self.run_task("classification", [],
+                                  transients=outputs['transients']))
+                self.run_task("alerts", [],
+                              transients=outputs['transients'])
+                self.run_task("prettyprint", [], transients=outputs['transients'])
+
+        dataset.process_ts = datetime.datetime.utcnow()
+        database.close()
+
+
+if __name__ == '__main__':
+    sys.exit(SIP().main())

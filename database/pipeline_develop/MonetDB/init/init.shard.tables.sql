6063ec29246c24e2df1fd051678b6b4fa09df3ae
Bart Scheers <L.H.A.Scheers@uva.nl>
2012-02-13 14:51:45 +0000

    buildnodes is taken care of in set-up script
    git-svn-id: http://svn.transientskp.org/code/database/trunk@2704 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

---
+++ database/pipeline_develop/MonetDB/init/init.shard.tables.sql
@@ -0,0 +1,18 @@
+--SELECT NOW();
+CALL InsertVersion();
+
+--SELECT NOW();
+--CALL BuildZones(1);
+
+--SELECT NOW();
+CALL BuildFrequencyBands();
+
+--CALL BuildNodes(-90,89);
+
+--SELECT NOW();
+--CALL BuildAssociationClass();
+
+--SELECT NOW();
+--CALL LoadLSM(158,164.5,18.5,24.5,'NVSS','VLSS','');
+
+

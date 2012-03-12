60e8fa92ec2fb853145b5154a7e31034c638b143
Evert Rol <evert.astro@gmail.com>
2012-03-12 15:43:42 +0000

    Updated trap-with-trip to use the awimager recipe
    git-svn-id: http://svn.transientskp.org/code/trap/trunk@2783 2b73c8c1-3922-0410-90dd-bc0a5c6f2ac6

--- recipes/trap-with-trip.py
+++ recipes/trap-with-trip.py
@@ -5,6 +5,7 @@ from __future__ import with_statement
 import sys
 import os
 import datetime
+from itertools import repeat
 
 from pyrap.quanta import quantity
 
@@ -28,7 +29,7 @@ class SIP(control):
             self.logger.info('storage mapfile = %s' % storage_mapfile)
             
             # Produce a GVDS file describing the data on the storage nodes.
-            self.run_task('vdsmaker', storage_mapfile)
+            gvds_file = self.run_task('vdsmaker', storage_mapfile)['gvds']
             
             # Read metadata (start, end times, pointing direction) from GVDS.
             vdsinfo = self.run_task("vdsreader")
@@ -39,28 +40,35 @@ class SIP(control):
             ndppp_results = self.run_task(
                 "ndppp",
                 storage_mapfile,
-            )            
-            compute_mapfile = ndppp_results['mapfile']
-            self.logger.info("compute map file = %s", compute_mapfile)
-            
-            ra = quantity(vdsinfo['pointing']['ra']).get_value('deg')
-            dec = quantity(vdsinfo['pointing']['dec']).get_value('deg')
-            central = self.run_task(
-                "skymodel", ra=ra, dec=dec, search_size=2.5
-                )
+            )
+
+            # Remove baselines which have been fully-flagged in any
+            # individual subband.
+            compute_mapfile = self.run_task(
+                "flag_baseline",
+                ndppp_results['mapfile'],
+                baselines=ndppp_results['fullyflagged']
+                )['mapfile']
+                                    
+
+            #ra = quantity(vdsinfo['pointing']['ra']).get_value('deg')
+            #dec = quantity(vdsinfo['pointing']['dec']).get_value('deg')
+            #central = self.run_task(
+            #    "skymodel", ra=ra, dec=dec, search_size=2.5
+            #    )
 
             parmdb_mapfile = self.run_task("parmdb", compute_mapfile)['mapfile']
             sourcedb_mapfile = self.run_task("sourcedb", compute_mapfile)['mapfile']
-            
-            # Patch the name of the central source into the BBS parset for
-            # subtraction.
+#            
+#            # Patch the name of the central source into the BBS parset for
+#            # subtraction.
             with patched_parset(
                 self.task_definitions.get("bbs", "parset"),
                 {
-                'BBSControl.Step.correct.Model.Sources': '[ "%s" ]' % (central["source_name"]),
-                'BBSControl.Step.solve1.Model.Sources': '[ "%s" ]' % (central["source_name"]),
-                'BBSControl.Step.solve2.Model.Sources': '[ "%s" ]' % (central["source_name"]),
-                'BBSControl.Step.subtract.Model.Sources': '[ "%s" ]' % (central["source_name"])
+                ## 'Step.correct.Model.Sources': '[ "%s" ]' % (central["source_name"]),
+                ## 'Step.solve1.Model.Sources': '[ "%s" ]' % (central["source_name"]),
+                ## 'Step.solve2.Model.Sources': '[ "%s" ]' % (central["source_name"]),
+                ## 'Step.subtract.Model.Sources': '[ "%s" ]' % (central["source_name"])
                 }
                 ) as bbs_parset:
                 self.logger.info("bbs patched parset = %s" % bbs_parset)
@@ -71,17 +79,32 @@ class SIP(control):
                     parset=bbs_parset,
                     instrument_mapfile=parmdb_mapfile,
                     sky_mapfile=sourcedb_mapfile)['mapfile']
-                #self.run_task("bbs", compute_mapfile, parset=bbs_parset)
-            
-            # rerun DPPP on calibrated data
-            ndppp_results = self.run_task("ndppp2", compute_mapfile)
-            compute_mapfile = ndppp_results['mapfile']
+
+            # Now, run DPPP three times on the output of BBS. We'll run
+            # this twice: once on CORRECTED_DATA, and once on
+            # SUBTRACTED_DATA.
+            with patched_parset(
+                os.path.join(
+                self.config.get("layout", "parset_directory"),
+                "ndppp.postbbs.parset"
+                ),
+                {
+                # "clip1.amplmax": str(5 * central["source_flux"])
+                },
+                output_dir=self.config.get("layout", "parset_directory")
+                ) as corrected_ndppp_parset:
+                for i in repeat(None, 2):
+                    self.run_task(
+                        "ndppp",
+                        compute_mapfile,
+                        parset=corrected_ndppp_parset,
+                        suffix=""
+                        )
             # Produce a GVDS file describing the data on the storage nodes.
             gvds_file = self.run_task('vdsmaker', compute_mapfile)['gvds']
             self.logger.info("GVDS file = %s" % gvds_file)
-            #self.run_task('cimager', compute_mapfile)
             
-            # Create the dataset
+            # Create the dataset in the database
             database = DataBase()
             dataset = DataSet(data={'dsinname': self.inputs['job_name']},
                               database=database)
@@ -90,7 +113,7 @@ class SIP(control):
             mapfiles = outputs['mapfiles']
             subdirs = ["%d" % int(starttime) for starttime, endtime in
                        outputs['timesteps']]
-            self.logger.info("subdirs = %s", str(subdirs))
+            self.logger.info("directories with sliced MSs = %s", str(subdirs))
             for iteration, (mapfile, subdir) in enumerate(zip(mapfiles,
                                                             subdirs)):
                 self.logger.info("Starting time slice iteration #%d" %
@@ -101,17 +124,17 @@ class SIP(control):
                     self.inputs['job_name'],
                     subdir
                     )
-                outputs = self.run_task('cimager_trap', mapfile,
-                                        vds_dir=os.path.dirname(mapfile),
-                                        results_dir=results_dir)
-
+                outputs = self.run_task('awimager', mapfile)
+            
                 outputs.update(
                     self.run_task('img2fits', images=outputs['images'],
                         results_dir=os.path.join(
                             self.config.get('layout', 'results_directory'),
                             subdir))
                     )
-
+                outputs.update(self.run_task("source_extraction",
+                                             images=[outputs['combined_fitsfile']],
+                                             dataset_id=dataset.id))
                 outputs.update(
                     self.run_task("monitoringlist",
                                   image_ids=outputs['image_ids'],

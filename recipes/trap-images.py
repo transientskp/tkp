daf0e468db7fa21ccc7012d8ccd4343266f4eaa6
Tim Staley <tim.staley@soton.ac.uk>
2012-07-30 18:14:32 +0100

    Refactor trap-images with addition of manual monitor coords.

--- recipes/trap-images.py
+++ recipes/trap-images.py
@@ -13,13 +13,14 @@ import datetime
 
 from pyrap.quanta import quantity
 
+import lofarpipe.support
 from lofarpipe.support.control import control
 from lofarpipe.support.utilities import log_time
 from lofarpipe.support.parset import patched_parset
 import lofarpipe.support.lofaringredient as ingredient
 
-from tkp.database.database import DataBase
-from tkp.database.dataset import DataSet
+from tkp.database import DataBase
+from tkp.database import DataSet
 
 
 
@@ -30,35 +31,40 @@ class TrapImages(control):
             help='Specify a previous dataset id to append the results to.',
             default=-1
             ),
+        'monitor_coords': ingredient.ListField(
+            '--monitor-coords',
+            #Unfortunately the ingredient system cannot handle spaces in 
+            #parameter fields
+            # I have tried enclosing with quotes, switching to StringField, 
+            #still no good.
+            help='Specify a list of RA,DEC co-ordinates to monitor\n' 
+                '(decimal degrees, no spaces), e.g.:\n'
+                '--monitor-coords=[137.01,14.02,137.05,15.01]',
+            default=[]
+            ),
       }
     
     def pipeline_logic(self):
-        from images_to_process import images
-        
+        from images_to_process import images        
         # Create the dataset
         database = DataBase()
-        if self.inputs['dataset_id'] == -1:
-            dataset = DataSet(data={'description': self.inputs['job_name']},
-                              database=database)
-        else:
-            dataset = DataSet(id = self.inputs['dataset_id'],
-                              database=database)
-            self.logger.info("Appending results to previous dataset")
-            
-        self.logger.info("dataset id = %d", dataset.id)
+        dataset = self.initialise_dataset(database)
+        self.add_manual_monitoringlist_entries(dataset)
+        
         with log_time(self.logger):
             if len(images):
                 self.logger.info("Processing images ...")
                 outputs = self.run_task("source_extraction", images,
                                                 dataset_id=dataset.id,
-    #                                            nproc = self.config.get('DEFAULT', 'default_nproc')
-                                                nproc=1 #Force nproc =1 until issue #3357 is fixed.
-                                        )
-    
+#                                   nproc = self.config.get('DEFAULT', 'default_nproc')
+                                    nproc=1 #Force nproc =1 until issue #3357 is fixed.
+                                    )
                 outputs.update(
                     self.run_task("monitoringlist", 
                                   [dataset.id],
-                                  nproc=1))
+                                  nproc=1 #Force nproc =1 until certain concurrent database access OK.
+                                  )
+                               ) 
                 outputs.update(
                         self.run_task("transient_search", [dataset.id],
                                        image_ids=outputs['image_ids']))
@@ -75,5 +81,47 @@ class TrapImages(control):
         database.close()
 
 
+
+    def initialise_dataset(self, database):
+        """Either inits a fresh dataset, or grabs the dataset specified 
+            at command line"""
+        if self.inputs['dataset_id'] == -1:
+            dataset = DataSet(data={'description': self.inputs['job_name']},
+                              database=database)
+        else:
+            dataset = DataSet(id = self.inputs['dataset_id'],
+                              database=database)
+            self.logger.info("Appending results to previously entered dataset")
+            
+        self.logger.info("dataset id = %d", dataset.id)
+        return dataset
+    
+    def add_manual_monitoringlist_entries(self, dataset):
+        """Parses co-ords from self.inputs, loads them into the monitoringlist"""
+        mon_coords = self.parse_monitoringlist_coords()
+        for c in mon_coords:
+            dataset.add_manual_entry_to_monitoringlist(c[0],c[1])
+    
+    
+    def parse_monitoringlist_coords(self):
+        """Returns a list of coord 2-tuples, format is: [(RA,DEC)] """
+        if len(self.inputs['monitor_coords']):
+            raw_monitor_list =  self.inputs['monitor_coords']
+            
+            if len(raw_monitor_list)%2 != 0:
+                raise ValueError("Odd number of monitor co-ordinates supplied: "
+                "please supply RA,DEC pairs *with commas but no spaces*.")
+            ra_list = raw_monitor_list[0::2]
+            dec_list = raw_monitor_list[1::2]
+            monitor_coords = zip(ra_list, dec_list)
+            
+            print "You specified monitoring at coords:"
+            for i in monitor_coords:
+                print "RA,", i[0]," ; Dec, " , i[1]
+            return monitor_coords
+        return []
+    
+    
+            
 if __name__ == '__main__':
     sys.exit(TrapImages().main())

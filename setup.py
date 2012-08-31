2f405486d620d56076df5feec01d609a6216b5e8
Gijs Molenaar <gijs@pythonic.nl>
2012-08-31 12:25:53 +0200

    now installable with cmake and distutils

--- setup.py
+++ setup.py
@@ -1,45 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import os
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-django_dir = 'trap'
-
-for dirpath, dirnames, filenames in os.walk(django_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 setup(
-    name="TKP recipes",
+    name="trap",
     version="0.1-dev",
-    packages=packages
-    description="LOFAR Transients Key Project pipeline recipes",
+    packages=['trap',
+              'trap.conf',
+              'trap.recipes',
+              'trap.recipes.nodes',
+              'trap.recipes.master',
+              'trap.bin',
+              'trap.management'
+    ],
+    description="LOFAR Transients pipeline (TRAP)",
     author="TKP Discovery WG",
     author_email="discovery@transientskp.org",
     url="http://www.transientskp.org/",

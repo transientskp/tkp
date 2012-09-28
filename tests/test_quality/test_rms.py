f4f4502114c7ddec8c31b257ece3274ebb27ccc7
Gijs Molenaar <gijs@pythonic.nl>
2012-09-28 14:54:30 +0200

    cleanup of noise/rms testing

--- tests/test_quality/test_rms.py
+++ tests/test_quality/test_rms.py
@@ -8,6 +8,7 @@ import sys
 import re
 import math
 from tkp.utility import accessors
+import tkp.quality
 from tkp.quality import statistics
 import tkp.lofar.noise
 import tkp.config
@@ -19,24 +20,26 @@ from numpy.testing import assert_array_equal, assert_almost_equal
 
 
 DATAPATH = tkp.config.config['test']['datapath']
-bad_file = os.path.join(DATAPATH, 'quality/noise/bad/home-pcarrol-msss-3C196a-analysis-band5.corr.fits')
-good_file = os.path.join(DATAPATH, 'quality/noise/good/home-pcarrol-msss-L086+69-analysis-band2.corr.fits')
-antenna_file = os.path.join(DATAPATH, 'lofar/CS001-AntennaArrays.conf')
+
+core_antennas = os.path.join(DATAPATH, 'lofar/CS001-AntennaArrays.conf')
+intl_antennas = os.path.join(DATAPATH, 'lofar/DE601-AntennaArrays.conf')
+remote_antennas = os.path.join(DATAPATH, 'lofar/RS106-AntennaArrays.conf')
+
+bad_files = [os.path.join(DATAPATH, 'quality/noise/bad/home-pcarrol-msss-3C196a-analysis-band%i.corr.fits' % i) for i in range(1, 8)]
+good_files = [os.path.join(DATAPATH, 'quality/noise/good/home-pcarrol-msss-L086+69-analysis-band%i.corr.fits' % i) for i in range(2, 8)]
 
 #numpy.seterr(all='raise')
 
-@requires_data(antenna_file)
-@requires_data(bad_file)
-@requires_data(good_file)
+@requires_data(core_antennas)
+@requires_data(bad_files[0])
 class TestNoise(unittest.TestCase):
-    def setUp(self):
-        self.bad_image = accessors.FitsFile(bad_file, plane=0)
-        self.good_image = accessors.FitsFile(good_file, plane=0)
-
     def testRms(self):
+        """ basic check if RMS calculation works
+        """
         self.assertEquals(statistics.rms(numpy.ones([4,4])*4), 0)
 
     def testClip(self):
+        """ basic check if clipping works"""
         a = numpy.ones([50, 50]) * 10
         a[20, 20] = 20
         clipped = statistics.clip(a)
@@ -44,13 +47,14 @@ class TestNoise(unittest.TestCase):
         assert_array_equal(clipped,  check)
 
     def testTheoreticalMaxValue(self):
-        frequency = self.good_image.freqeff
+        good_image = accessors.FitsFile(good_files[0], plane=0)
+        frequency = good_image.freqeff
 
         #integration_time = self.good_image.inttime
         integration_time = 18654.3 # s
 
         #bandwidth = self.good_image.freqbw
-        subbandwidth = 200 * 10**3 # Hz
+        subbandwidth = 200 * 10**6 # Hz
 
         ncore = 23 # ~
         nremote = 8 # ~
@@ -59,33 +63,47 @@ class TestNoise(unittest.TestCase):
         num_channels = 64
         configuration = "LBA_INNER"
 
-        # extract pixel size from HISTORY junk in fits header
-        history = self.bad_image.header['HISTORY*']
-        xy_line = [i for i in history.values() if 'cellx' in i][0]
-        extracted = re.search(r"cellx='(?P<x>\d+).*celly='(?P<y>\d+)", xy_line).groupdict()
-        xsize = int(extracted['x'])
-        ysize = int(extracted['y'])
-        bmaj = self.bad_image.header['BMAJ'] * 3600 # convert from degrees to arcsec
-        bmin = self.bad_image.header['BMIN'] * 3600
-
-        bmaj_scaled = bmaj / xsize
-        bmin_scaled = bmin / ysize
-        area = math.pi * bmaj_scaled * bmin_scaled
-
         noise_level = tkp.lofar.noise.noise_level(frequency, subbandwidth, integration_time, configuration, num_subband,
                         num_channels, ncore, nremote, nintl)
 
+        # TODO: find a nice way to test this
         print noise_level
 
     def testRmsFits(self):
-        bad_rms = statistics.rms(self.bad_image.data)
-        good_rms = statistics.rms(self.good_image.data)
+        # TODO: find a nice way to test this
+        for bad_file in bad_files:
+            bad_image = accessors.FitsFile(bad_file, plane=0)
+            beam_correction = statistics.beam_correction(bad_image)
+            bad_rms = statistics.rms(bad_image.data)
+            #print bad_file, bad_rms, bad_rms/beam_correction
+
+        for good_file in good_files:
+            good_image = accessors.FitsFile(good_file, plane=0)
+            beam_correction = statistics.beam_correction(good_image)
+            good_rms = statistics.rms(good_image.data)
+            #print good_file, good_rms, good_rms/beam_correction
+
 
     def testDistanses(self):
-        parsed = tkp.lofar.antennaarrays.parse_antennafile(antenna_file)
+        """
+        check if all precomputed values match with distances in files
+        """
+        parsed = tkp.lofar.antennaarrays.parse_antennafile(core_antennas)
         for conf in "LBA", "LBA_INNER", "LBA_OUTER", "LBA_SPARSE0", "LBA_SPARSE1":
             ds = tkp.lofar.antennaarrays.shortest_distances(parsed[conf], parsed["LBA"])
-            assert_almost_equal(ds, tkp.lofar.antennaarrays.dipole_distances[conf], decimal=2)
+            assert_almost_equal(ds, tkp.lofar.antennaarrays.core_dipole_distances[conf], decimal=2)
+
+        parsed = tkp.lofar.antennaarrays.parse_antennafile(intl_antennas)
+        for conf in "LBA", "LBA_INNER", "LBA_OUTER":
+            ds = tkp.lofar.antennaarrays.shortest_distances(parsed[conf], parsed["LBA"])
+            assert_almost_equal(ds, tkp.lofar.antennaarrays.intl_dipole_distances[conf], decimal=2)
+
+        parsed = tkp.lofar.antennaarrays.parse_antennafile(remote_antennas)
+        for conf in "LBA", "LBA_INNER", "LBA_OUTER", "LBA_SPARSE0", "LBA_SPARSE1", "LBA_X", "LBA_X":
+            ds = tkp.lofar.antennaarrays.shortest_distances(parsed[conf], parsed["LBA"])
+            assert_almost_equal(ds, tkp.lofar.antennaarrays.remote_dipole_distances[conf], decimal=2)
+
+
 
 if __name__ == '__main__':
     unittest.main()

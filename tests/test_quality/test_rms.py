5ed747d332732ca83c7044eb5b8ff7e9d4500eaa
Gijs Molenaar <gijs@pythonic.nl>
2012-10-23 12:30:32 +0200

    cleanup

--- tests/test_quality/test_rms.py
+++ tests/test_quality/test_rms.py
@@ -1,23 +1,21 @@
-__author__ = 'Gijs Molenaar'
-
 import unittest
 if not  hasattr(unittest.TestCase, 'assertIsInstance'):
     import unittest2 as unittest
 import os
 import sys
-import re
-import math
+
+import numpy
+from numpy.testing import assert_array_equal, assert_almost_equal
+
 from tkp.utility import accessors
 import tkp.quality
 from tkp.quality import statistics
-import tkp.database as tkpdb
 import tkp.lofar.noise
 import tkp.config
 import tkp.lofar.antennaarrays
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from decorators import requires_data
-import numpy
-from numpy.testing import assert_array_equal, assert_almost_equal
 
 
 DATAPATH = tkp.config.config['test']['datapath']
@@ -33,34 +31,28 @@ good_files = [os.path.join(DATAPATH, 'quality/noise/good/home-pcarrol-msss-L086+
 
 @requires_data(core_antennas)
 @requires_data(bad_files[0])
-class TestNoise(unittest.TestCase):
-    def testSubrgion(self):
+class TestRms(unittest.TestCase):
+    def test_subrgion(self):
         sub = statistics.subregion(numpy.ones((800, 800)))
         self.assertEqual(sub.shape, (400, 400))
 
-    def testRms(self):
-        """ basic check if RMS calculation works
-        """
+    def test_rms(self):
         self.assertEquals(statistics.rms(numpy.ones([4,4])*4), 0)
 
-    def testClip(self):
-        """ basic check if clipping works"""
+    def test_clip(self):
         a = numpy.ones([50, 50]) * 10
         a[20, 20] = 20
         clipped = statistics.clip(a)
         check = numpy.array([10] * (50*50-1))
         assert_array_equal(clipped,  check)
 
-    def testTheoreticalMaxValue(self):
+    def test_theoreticalnoise(self):
         good_image = accessors.FitsFile(good_files[0], plane=0)
         frequency = good_image.freqeff
 
-        #integration_time = self.good_image.inttime
-        integration_time = 18654.3 # s
-
-        #bandwidth = self.good_image.freqbw
-        subbandwidth = 200 * 10**6 # Hz
-
+        # this stuff should be in the header of a LOFAR image some day
+        integration_time = 18654.3 # s, should be self.good_image.inttime some day
+        subbandwidth = 200 * 10**6 # Hz, shoud probably be self.good_image.freqbw some day
         ncore = 23 # ~
         nremote = 8 # ~
         nintl = 0
@@ -68,28 +60,24 @@ class TestNoise(unittest.TestCase):
         num_channels = 64
         configuration = "LBA_INNER"
 
-        noise_level = tkp.lofar.noise.noise_level(frequency, subbandwidth, integration_time, configuration, num_subband,
+        noise = tkp.lofar.noise.noise_level(frequency, subbandwidth, integration_time, configuration, num_subband,
                         num_channels, ncore, nremote, nintl)
+        rms = statistics.rms(good_image.data)
 
-        # TODO: find a nice way to test this
-        print noise_level
+        # Todo: this test fails, since the RMS value is higher than the theoretical noise. hm...
+        #self.assertTrue(tkp.quality.rms_valid(rms, noise))
 
-    def testRmsFits(self):
-        # TODO: find a nice way to test this
+    def test_rms_fits(self):
         for bad_file in bad_files:
             bad_image = accessors.FitsFile(bad_file, plane=0)
             bad_rms = statistics.rms(bad_image.data)
-            #print bad_file, bad_rms
 
         for good_file in good_files:
             good_image = accessors.FitsFile(good_file, plane=0)
             good_rms = statistics.rms(good_image.data)
-            #print good_file, good_rms
 
-
-    def testDistanses(self):
-        """check if all precomputed values match with distances in files
-        """
+    def test_distanses(self):
+        """check if all precomputed values match with distances in files"""
         parsed = tkp.lofar.antennaarrays.parse_antennafile(core_antennas)
         for conf in "LBA", "LBA_INNER", "LBA_OUTER", "LBA_SPARSE0", "LBA_SPARSE1":
             ds = tkp.lofar.antennaarrays.shortest_distances(parsed[conf], parsed["LBA"])
@@ -104,8 +92,6 @@ class TestNoise(unittest.TestCase):
         for conf in "LBA", "LBA_INNER", "LBA_OUTER", "LBA_SPARSE0", "LBA_SPARSE1", "LBA_X", "LBA_X":
             ds = tkp.lofar.antennaarrays.shortest_distances(parsed[conf], parsed["LBA"])
             assert_almost_equal(ds, tkp.lofar.antennaarrays.remote_dipole_distances[conf], decimal=2)
-
-
 
 if __name__ == '__main__':
     unittest.main()
--- tkp/quality/statistics.py
+++ tkp/quality/statistics.py
@@ -2,21 +2,21 @@
 functions for calculating statistical properties of LOFAR images
 """
 import numpy
-import re
-import math
 
 def rms(data):
-    """
-    returns the clipped RMS of an image
+    """returns the clipped RMS of an image
+    Args:
+        data: a numpy array
     """
     clipped_data = clip(data.ravel())
     clipped_data -= numpy.median(clipped_data)
     return numpy.sqrt(numpy.power(clipped_data, 2).sum()/len(clipped_data))
 
 def clip(data, sigma=3):
-    """
-    remove all values above a threshold from the array
+    """remove all values above a threshold from the array.
     uses iterative clipping at sigma value until nothing more is getting clipped.
+    Args:
+        data: a numpy array
     """
     median = numpy.median(data)
     std = numpy.std(data)
@@ -27,9 +27,10 @@ def clip(data, sigma=3):
         return newdata
 
 def subregion(data, f=4):
-    """
-    returns the inner region of a image, according to f.
+    """returns the inner region of a image, according to f.
     if f is for example 4, the returned region will be 1/4th of the total.
+    Args:
+        data: a numpy array
     """
     x,y = data.shape
     return data[(x/2 - x/f):(x/2 + x/f), (y/2 - y/f):(y/2 + y/f)]

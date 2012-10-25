3dc4139dc596bd83b94193485566302eab1153b9
Gijs Molenaar <gijs@pythonic.nl>
2012-10-25 16:57:49 +0200

    using empty reciepe now

--- trap/recipes/master/quality_check.py
+++ trap/recipes/master/quality_check.py
@@ -1,6 +1,16 @@
 
 from lofarpipe.support.baserecipe import BaseRecipe
 from lofarpipe.support.remotecommand import RemoteCommandRecipeMixIn
+import lofarpipe.support.lofaringredient as ingredient
 
-class QualityCheck(BaseRecipe, RemoteCommandRecipeMixIn):
-    pass
+class quality_check(BaseRecipe, RemoteCommandRecipeMixIn):
+    inputs = {
+        'dataset_id': ingredient.IntField(
+            '--dataset-id',
+            help='Dataset to which images belong',
+            default=None
+        ),
+    }
+    outputs = {
+        'image_ids': ingredient.ListField()
+    }
--- trap/recipes/nodes/quality_check.py
+++ trap/recipes/nodes/quality_check.py
@@ -1,5 +1,5 @@
 
 from lofarpipe.support.lofarnode import LOFARnodeTCP
 
-class QualityCheck(LOFARnodeTCP):
+class quality_check(LOFARnodeTCP):
     pass

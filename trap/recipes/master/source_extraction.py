e18f6b85198db072c0885597d741336a8290368f
Gijs Molenaar <gijs@pythonic.nl>
2012-10-30 15:25:06 +0100

    removed mongo stuff

--- trap/recipes/master/source_extraction.py
+++ trap/recipes/master/source_extraction.py
@@ -20,37 +20,13 @@ class source_extraction(BaseRecipe, RemoteCommandRecipeMixIn):
             dest='parset',
             help="Source finder configuration parset"
         ),
-        'dataset_id': ingredient.IntField(
-            '--dataset-id',
-            help='Dataset to which images belong',
-            default=None
-        ),
         'nproc': ingredient.IntField(
             '--nproc',
             help="Maximum number of simultaneous processes per compute node",
             default=8
         ),
-        'store_images': ingredient.BoolField(
-            '--store-images',
-            help="Store images in MongoDB database",
-            default=False
-        ),
-        'mongo_host': ingredient.StringField(
-            '--mongo-host',
-            help="MongoDB hostname",
-            default="pc-swinbank.science.uva.nl"
-        ),
-        'mongo_port': ingredient.IntField(
-            '--mongo-port',
-            help="MongoDB port number",
-            default=27017
-        ),
-        'mongo_db': ingredient.StringField(
-            '--mongo-db',
-            help="MongoDB database",
-            default="tkp"
-        )
     }
+
     outputs = {
         'image_ids': ingredient.ListField()
     }
@@ -60,7 +36,6 @@ class source_extraction(BaseRecipe, RemoteCommandRecipeMixIn):
         super(source_extraction, self).go()
         images = self.inputs['args']
         print 'IMAGES =', images
-        dataset_id = self.inputs['dataset_id']
 
         # Obtain available nodes
         clusterdesc = ClusterDesc(self.config.get('cluster', "clusterdesc"))
@@ -89,22 +64,15 @@ class source_extraction(BaseRecipe, RemoteCommandRecipeMixIn):
                     host, command,
                     arguments=[
                         image,
-#                        self.inputs['detection_level'],
-                        dataset_id,
-#                        self.inputs['radius'],
                         self.inputs['parset'],
-                        self.inputs['store_images'],
-                        self.inputs['mongo_host'],
-                        self.inputs['mongo_port'],
-                        self.inputs['mongo_db'],
                         tkp.config.CONFIGDIR
                     ]
                 )
             )
         jobs = self._schedule_jobs(jobs, max_per_node=self.inputs['nproc'])
         self.outputs['image_ids'] = [job.results['image_id'] for job in jobs.itervalues()]
-        #                Check if we recorded a failing process before returning
-        # ----------------------------------------------------------------------
+
+        # Check if we recorded a failing process before returning
         if self.error.isSet():
             self.logger.warn("Failed source extraction process detected")
             return 1
--- trap/recipes/nodes/source_extraction.py
+++ trap/recipes/nodes/source_extraction.py
@@ -3,26 +3,16 @@ from lofarpipe.support.lofarnode import LOFARnodeTCP
 import trap.source_extraction
 
 class source_extraction(LOFARnodeTCP):
-    def run(self, image, dataset_id, parset, store_images, mongo_host, mongo_port, mongo_db, tkpconfigdir=None):
+    def run(self, image,  parset, tkpconfigdir=None):
         """Extract sources from a FITS image
         Args:
-
             - image: FITS filename
-
-            - dataset_id: dataset to which image belongs
-
             - parset: parameter set *filename* containg at least the
                   detection threshold and the source association
                   radius, the last one in units of the de Ruiter
                   radius.
-
-            - storage_images: bool. Store images to MongoDB database if True.
-
-            - mongo_host/port/db: details of MongoDB to use if store_images is
-              True.
         """
-        self.outputs['image_id'] = trap.source_extraction.extract_sources(image, dataset_id, parset, store_images,
-                                            mongo_host, mongo_port, mongo_db, tkpconfigdir)
+        self.outputs['image_id'] = trap.source_extraction.extract_sources(image, parset, tkpconfigdir)
 
         return 0
 

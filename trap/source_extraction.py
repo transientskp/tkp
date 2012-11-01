191b327b91de974e65afb9d4d4261c8366a915ae
Gijs Molenaar <gijs@pythonic.nl>
2012-11-01 17:04:47 +0100

    adepted old style to prevent problems

--- trap/source_extraction.py
+++ trap/source_extraction.py
@@ -1,11 +1,9 @@
 import os
 import logging
+from contextlib import closing
 from lofarpipe.support.lofarexceptions import PipelineException
 from lofarpipe.support.utilities import log_time
 from lofar.parameterset import parameterset
-from tkp.database import DataBase
-from tkp.utility.accessors import FITSImage
-from tkp.utility.accessors import sourcefinder_image_from_accessor
 from tkp.database.orm import Image
 
 logger = logging.getLogger(__name__)
@@ -20,53 +18,62 @@ def extract_sources(image_id, parset, tkpconfigdir=None):
               radius, the last one in units of the de Ruiter
               radius.
     """
-
-    log_time(logger)
-
     if tkpconfigdir:   # allow nodes to pick up the TKPCONFIGDIR
         os.environ['TKPCONFIGDIR'] = tkpconfigdir
+    import tkp
     from tkp.config import config
+    from tkp.database import DataBase
+    from tkp.utility.accessors import FITSImage
+    from tkp.utility.accessors import sourcefinder_image_from_accessor
 
-    database = DataBase()
-    db_image = Image(database=database, id=image_id)
-    if not os.path.exists(db_image.url):
-        raise PipelineException("Image '%s' not found" % db_image.url)
-    fitsimage = FITSImage(db_image.url)
-    seconfig = config['source_extraction']
-    parset = parameterset(parset)
+    with log_time(logger):
+        with closing(DataBase()) as database:
+            db_image = Image(database=database, id=image_id)
+            if not os.path.exists(db_image.url):
+                raise PipelineException("Image '%s' not found" % db_image.url)
+            fitsimage = FITSImage(db_image.url)
+            seconfig = config['source_extraction']
+            parset = parameterset(parset)
 
-    logger.info("Detecting sources in %s at %f level", db_image.url, parset.getFloat('detection.threshold'))
-    data_image = sourcefinder_image_from_accessor(fitsimage)
-    seconfig['back_sizex'] = parset.getInt('backsize.x', seconfig['back_sizex'])
-    seconfig['back_sizey'] = parset.getInt('backsize.y', seconfig['back_sizey'])
-    seconfig['margin'] = parset.getFloat('margin', seconfig['margin'])
-    seconfig['deblend'] = parset.getBool('deblend', seconfig['deblend'])
-    seconfig['deblend_nthresh'] = parset.getInt('deblend_nthresh', seconfig['deblend_nthresh'])
+            logger.info("Detecting sources in %s at %f level", image_id, parset.getFloat('detection.threshold'))
 
-    logger.info("Employing margin: %f, deblend: %s, deblend_nthresh:%d",
-        seconfig['margin'],
-        seconfig['deblend'],
-        seconfig['deblend_nthresh']
-    )
+            data_image = sourcefinder_image_from_accessor(fitsimage)
 
-    det = parset.getFloat('detection.threshold',
-        seconfig['detection_threshold'])
-    anl = parset.getFloat('analysis.threshold',
-        seconfig['analysis_threshold'])
+            seconfig['back_sizex'] = parset.getInt('backsize.x',
+                seconfig['back_sizex'])
+            seconfig['back_sizey'] = parset.getInt('backsize.y',
+                seconfig['back_sizey'])
+            seconfig['margin'] = parset.getFloat('margin',
+                seconfig['margin'])
+            seconfig['deblend'] = parset.getBool('deblend',
+                seconfig['deblend'])
+            seconfig['deblend_nthresh'] = parset.getInt('deblend_nthresh',
+                seconfig['deblend_nthresh'])
 
-    ##Finally, do some work!
-    results = data_image.extract(det=det, anl=anl)
 
-    logger.info("Detected %d sources", len(results))
-    logger.info("Saving extracted sources to database")
-    tuple_results = [result.serialize() for result in results]
-    db_image.insert_extracted_sources(tuple_results)
-    deRuiter_r = (parset.getFloat('association.radius') *
-                  config['source_association']['deruiter_radius'])
-    logger.info("Associate newly extracted sources with existing ones")
-    db_image.associate_extracted_sources(deRuiter_r=deRuiter_r)
-    #self.logger.info("Update monitoring list for already found sources")
-    #db_image.match_monitoringlist(assoc_r=deRuiter_r, mindistance=30)
+            logger.info("Employing margin: %f, deblend: %s, deblend_nthresh:%d",
+                seconfig['margin'],
+                seconfig['deblend'],
+                seconfig['deblend_nthresh']
+            )
 
-    return db_image.id
+            det = parset.getFloat('detection.threshold',
+                seconfig['detection_threshold'])
+            anl = parset.getFloat('analysis.threshold',
+                seconfig['analysis_threshold'])
+
+            ##Finally, do some work!
+            results = data_image.extract(det=det, anl=anl)
+
+            logger.info("Detected %d sources", len(results))
+            logger.info("Saving extracted sources to database")
+            tuple_results = [result.serialize() for result in results]
+            db_image.insert_extracted_sources(tuple_results)
+            deRuiter_r = (parset.getFloat('association.radius') *
+                          config['source_association']['deruiter_radius'])
+            logger.info("Associate newly extracted sources with existing ones")
+            db_image.associate_extracted_sources(deRuiter_r=deRuiter_r)
+            #logger.info("Update monitoring list for already found sources")
+            #db_image.match_monitoringlist(assoc_r=deRuiter_r, mindistance=30)
+            return db_image.id
 

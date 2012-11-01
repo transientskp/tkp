0178ee9f3ccc7c571f1783fb740aac916f14be7b
Gijs Molenaar <gijs@pythonic.nl>
2012-11-01 17:03:19 +0100

    moved logic outside of recipes

--- trap/recipes/master/prettyprint.py
+++ trap/recipes/master/prettyprint.py
@@ -1,78 +1,15 @@
-from __future__ import with_statement
-
-"""
-
-Pretty print the classified transients
-
-"""
-
-__author__ = 'Evert Rol / TKP software group'
-__email__ = 'evert.astro@gmail.com'
-__contact__ = __author__ + ', ' + __email__
-__copyright__ = '2011, University of Amsterdam'
-__version__ = '0.1'
-__last_modification__ = 'feb 2011'
-
-
-import sys
-import os
-from operator import itemgetter
-
+import trap.prettyprint
 from lofarpipe.support.baserecipe import BaseRecipe
-from lofarpipe.support import lofaringredient
-from lofarpipe.cuisine.parset import Parset
-
 
 class prettyprint(BaseRecipe):
-
-    inputs = {
-    }
-
+    inputs = {}
     outputs = {}
 
     def go(self):
         super(prettyprint, self).go()
-        results = "\n====== Results ======\n"
-        for transient in self.inputs['args']:
-            indent = 8 * " "
-            features = ""
-            catalogs = ""
-            for key, value in sorted(transient.features.iteritems(),
-                                     key=itemgetter(1)):
-                try:
-                    features += "|" + indent + "%s: %.3f\n" % (key, value)
-                except TypeError:  # incorrect format argument
-                    features += "|" + indent + "%s: %s\n" % (key, str(value))
-            for key, value in sorted(transient.catalogs.iteritems(),
-                                     key=itemgetter(1)):
-                catalogs += "|" + indent + "%s: %s\n" % (key, str(value))
-            classification = ""
-            for key, value in sorted(transient.classification.iteritems(),
-                                     key=itemgetter(1), reverse=True):
-                classification += "|" + indent + "%s: %.1f\n" % (key, value)
-            results += """\
-\033[36m
-| transient #%d (dataset %s):
-|   T0: %s  %s
-|   position: %s
-|   features:
-%s
-|   catalogs:
-%s
-|   classification:
-%s
-\033[0m
-""" % (transient.runcatid, transient.dataset,
-       transient.timezero, transient.duration,
-       transient.position,
-       features, catalogs,
-       classification)
-
-        results += "\n--------------------\n"
-        self.logger.info(results)
-        
-        return 0
-
+        transients = self.inputs['args']
+        trap.prettyprint.prettyprint(transients)
 
 if __name__ == '__main__':
+    import sys
     sys.exit(prettyprint().main())
--- trap/recipes/nodes/classification.py
+++ trap/recipes/nodes/classification.py
@@ -1,57 +1,12 @@
-from __future__ import with_statement
-
-"""
-
-For info, see corresponding recipe in the ../master/ directory
-
-"""
-
-__author__ = 'Evert Rol / LOFAR Transients Key Project'
-__email__ = 'discovery@transientskp.org'
-__contact__ = __author__ + ', ' + __email__
-__copyright__ = '2010-2012, University of Amsterdam'
-__version__ = '0.2'
-__last_modification__ = '2012-01-20'
-
-
 import sys, os
-import traceback
-import imp
-
 from lofarpipe.support.lofarnode import LOFARnodeTCP
-from lofarpipe.support.utilities import log_time
-from lofar.parameterset import parameterset
-
+import trap.classification
 
 class classification(LOFARnodeTCP):
 
     def run(self, transient, parset, tkpconfigdir=None):
-        paths = [os.path.expanduser('~/.transientskp')]
-        if tkpconfigdir:   # allow nodes to pick up the TKPCONFIGDIR
-            os.environ['TKPCONFIGDIR'] = tkpconfigdir
-            paths.insert(0, tkpconfigdir)
-        from tkp.classification.manual.classifier import Classifier
-        from tkp.classification.transient import Transient
-
-        with log_time(self.logger):
-            try:
-                parset = parameterset(parset)
-                weight_cutoff = parset.getFloat('weighting.cutoff')
-
-                self.logger.info("Classifying transient #%d", transient.runcatid)
-                classifier = Classifier(transient, paths=paths)
-                results = classifier.classify()
-                transient.classification = {}
-                for key, value in results.iteritems():
-                    if value > weight_cutoff:
-                        transient.classification[key] = value
-            except Exception, e:
-                    self.logger.error(str(e))
-                    self.logger.error(traceback.format_exc())
-                    return 1
-        self.outputs['transient'] = transient
+        self.outputs['transient'] = trap.classification.classify(transient, parset, tkpconfigdir)
         return 0
-
 
 if __name__ == "__main__":
     jobid, jobhost, jobport = sys.argv[1:4]

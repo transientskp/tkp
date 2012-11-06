5b2de673bed6e44f3551ad8fa0a3eccd10c1df55
Gijs Molenaar <gijs@pythonic.nl>
2012-11-06 14:29:35 +0100

    fixes distributed logging

--- trap/monitoringlist.py
+++ trap/monitoringlist.py
@@ -6,7 +6,7 @@ from lofar.parameterset import parameterset
 from tkp.database import DataBase, DataSet
 from tkp.utility.accessors import FITSImage
 from tkp.utility.accessors import sourcefinder_image_from_accessor
-from lofarpipe.support.utilities import log_time
+
 from tkp.database.orm import Image
 
 logger = logging.getLogger(__name__)
@@ -29,26 +29,26 @@ def update_monitoringlist(image_id):
         - dataset_id: dataset to which the image belongs
     """
 
-    with log_time(logger):
-        with closing(DataBase()) as database:
-            db_image = Image(database=database, id=image_id)
-            if not os.path.exists(db_image.url):
-                raise PipelineException("Image '%s' not found" % db_image.url)
+
+    database = DataBase()
+    db_image = Image(database=database, id=image_id)
+    if not os.path.exists(db_image.url):
+        raise PipelineException("Image '%s' not found" % db_image.url)
 
 
-            # Obtain the list of sources to be monitored (and not already
-            # detected) for this image
-            fitsimage = FITSImage(db_image.url)
-            sources = db_image.monitoringsources()
+    # Obtain the list of sources to be monitored (and not already
+    # detected) for this image
+    fitsimage = FITSImage(db_image.url)
+    sources = db_image.monitoringsources()
 
-            # Run the source finder on these sources
-            if len(sources):
-                logger.info("Measuring %d undetected monitoring sources." % (len(sources),))
-                data_image = sourcefinder_image_from_accessor(fitsimage)
-                results = data_image.fit_fixed_positions(
-                    [(source[0], source[1]) for source in sources],
-                    boxsize=BOX_IN_BEAMPIX*max(data_image.beam[0], data_image.beam[1]))
-                # Filter out the bad ones, and combines with xtrsrc_ids
-                results = [(source[2], source[3], result.serialize()) for source, result in
-                           zip(sources, results) if result is not None]
-                db_image.insert_monitored_sources(results)
+    # Run the source finder on these sources
+    if len(sources):
+        logger.info("Measuring %d undetected monitoring sources." % (len(sources),))
+        data_image = sourcefinder_image_from_accessor(fitsimage)
+        results = data_image.fit_fixed_positions(
+            [(source[0], source[1]) for source in sources],
+            boxsize=BOX_IN_BEAMPIX*max(data_image.beam[0], data_image.beam[1]))
+        # Filter out the bad ones, and combines with xtrsrc_ids
+        results = [(source[2], source[3], result.serialize()) for source, result in
+                   zip(sources, results) if result is not None]
+        db_image.insert_monitored_sources(results)

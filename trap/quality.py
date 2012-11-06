ea94ba9b1bcb68201f161fdab3bae90b744e87fd
Gijs Molenaar <gijs@pythonic.nl>
2012-11-06 14:13:38 +0100

    used wrong logger

--- trap/quality.py
+++ trap/quality.py
@@ -8,7 +8,6 @@ from tkp.utility.accessors import FITSImage
 import tkp.database.quality
 import tkp.quality
 from tkp.database.orm import Image
-from contextlib import closing
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +29,12 @@ def parse_parset(parset_file):
     result['nintl'] = parset.getInt('nintl', 8)
     return result
 
+def nice_format(f):
+    if f > 9999 or f < 0.01:
+        return "%.2e" % f
+    else:
+        return "%.2f" % f
+
 def noise(image_id, parset_file):
     """ checks if an image passes the RMS quality check. If not, a rejection entry is added to the database.
     args:
@@ -39,22 +44,23 @@ def noise(image_id, parset_file):
         True if image passes tests, false if not
     """
 
-    with closing(DataBase()) as database:
-        db_image = Image(database=database, id=image_id)
-        fitsimage = FITSImage(db_image.url)
-        p = parse_parset(parset_file)
+    database = DataBase()
+    db_image = Image(database=database, id=image_id)
+    fitsimage = FITSImage(db_image.url)
+    p = parse_parset(parset_file)
 
-        rms = rms_with_clipped_subregion(fitsimage.data, sigma=p['sigma'], f=p['f'])
-        noise = noise_level(p['frequency'], p['subbandwidth'], p['intgr_time'],
-            p['configuration'], p['subbands'], p['channels'],
-            p['ncore'], p['nremote'], p['nintl'])
+    rms = rms_with_clipped_subregion(fitsimage.data, sigma=p['sigma'], f=p['f'])
+    noise = noise_level(p['frequency'], p['subbandwidth'], p['intgr_time'],
+        p['configuration'], p['subbands'], p['channels'],
+        p['ncore'], p['nremote'], p['nintl'])
 
-        if tkp.quality.rms_valid(rms, noise, low_bound=p['low_bound'], high_bound=p['high_bound']):
-            return True
-        else:
-            ratio = rms / noise
-            reason = "noise level is %s times theoretical value" % ratio
-            logger.info("image %s invalid: %s " % (db_image.id, reason) )
-            tkp.database.quality.reject(database.connection, db_image.id,
-                tkp.database.quality.reason['rms'], reason)
-            return False
+    if tkp.quality.rms_valid(rms, noise, low_bound=p['low_bound'], high_bound=p['high_bound']):
+        logger.info("image %i accepted: rms: %s, theoretical noise: %s" % (db_image.id, nice_format(rms), nice_format(noise)))
+        return True
+    else:
+        ratio = rms / noise
+        reason = "rms value (%s) is %s times theoretical noise (%s)" % (nice_format(rms), nice_format(ratio), nice_format(noise))
+        logger.info("image %s REJECTED: %s " % (db_image.id, reason) )
+        tkp.database.quality.reject(database.connection, db_image.id,
+            tkp.database.quality.reason['rms'], reason)
+        return False

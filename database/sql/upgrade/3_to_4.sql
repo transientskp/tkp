8508ca338edb130dba5980dd7b6125d0f9ea9378
Tim Staley <tim.staley@soton.ac.uk>
2012-11-14 17:10:26 +0000

    Split database changes into two versions.

--- database/sql/upgrade/3_to_4.sql
+++ database/sql/upgrade/3_to_4.sql
@@ -5,17 +5,3 @@ UPDATE version
 
 INSERT INTO rejectreason VALUES (0, 'RMS too high');
 
-ALTER TABLE transient ADD COLUMN band SMALLINT DEFAULT 0;
-ALTER TABLE transient ADD COLUMN v_int DOUBLE;
-ALTER TABLE transient ADD COLUMN eta_int DOUBLE;
-
-UPDATE transient 
-   SET v_int = v
-      ,eta_int = eta
-;
-
-ALTER TABLE transient DROP COLUMN v;
-ALTER TABLE transient DROP COLUMN eta;
-
-ALTER TABLE transient ADD FOREIGN KEY (band) REFERENCES frequencyband (id);
-
---
+++ database/sql/upgrade/4_to_5.sql
@@ -0,0 +1,19 @@
+UPDATE version 
+   SET value = 5
+ WHERE name = 'revision'
+;
+
+ALTER TABLE transient ADD COLUMN band SMALLINT DEFAULT 0;
+ALTER TABLE transient ADD COLUMN v_int DOUBLE;
+ALTER TABLE transient ADD COLUMN eta_int DOUBLE;
+
+UPDATE transient 
+   SET v_int = v
+      ,eta_int = eta
+;
+
+ALTER TABLE transient DROP COLUMN v;
+ALTER TABLE transient DROP COLUMN eta;
+
+ALTER TABLE transient ADD FOREIGN KEY (band) REFERENCES frequencyband (id);
+

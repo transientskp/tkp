dd9e48dc4feeda31a81fd1a33922f2ce7f0855fc
Gijs Molenaar <gijs@pythonic.nl>
2012-12-13 12:53:02 +0100

    adjusted insertImage function to support phasecentre
    Conflicts:
    	database/sql/tables/version.sql
    	database/sql/upgrade/5_to_6.sql
    	database/sql/upgrade/6_to_5.sql

--- database/sql/upgrade/5_to_6.sql
+++ database/sql/upgrade/5_to_6.sql
@@ -2,21 +2,22 @@ UPDATE version
    SET value = 6
  WHERE name = 'revision'
    AND value = 5
-;
+; %SPLIT%
 
-ALTER TABLE extractedsource ADD COLUMN ra_fit_err DOUBLE NULL;
-ALTER TABLE extractedsource ADD COLUMN decl_fit_err DOUBLE NULL;
-ALTER TABLE extractedsource ADD COLUMN ra_sys_err DOUBLE NULL;
-ALTER TABLE extractedsource ADD COLUMN decl_sys_err DOUBLE NULL;
+ALTER TABLE extractedsource ADD COLUMN ra_fit_err DOUBLE NULL; %SPLIT%
+ALTER TABLE extractedsource ADD COLUMN decl_fit_err DOUBLE NULL; %SPLIT%
+ALTER TABLE extractedsource ADD COLUMN ra_sys_err DOUBLE NULL; %SPLIT%
+ALTER TABLE extractedsource ADD COLUMN decl_sys_err DOUBLE NULL; %SPLIT%
 
 UPDATE extractedsource
    SET ra_fit_err = ra_err
       ,decl_fit_err = decl_err
       ,ra_sys_err = 0
       ,decl_sys_err = 0
-;
+; %SPLIT%
 
-ALTER TABLE extractedsource ALTER COLUMN ra_fit_err SET NOT NULL;
-ALTER TABLE extractedsource ALTER COLUMN decl_fit_err SET NOT NULL;
-ALTER TABLE extractedsource ALTER COLUMN ra_sys_err SET NOT NULL;
-ALTER TABLE extractedsource ALTER COLUMN decl_sys_err SET NOT NULL;
+ALTER TABLE extractedsource ALTER COLUMN ra_fit_err SET NOT NULL; %SPLIT%
+ALTER TABLE extractedsource ALTER COLUMN decl_fit_err SET NOT NULL; %SPLIT%
+ALTER TABLE extractedsource ALTER COLUMN ra_sys_err SET NOT NULL; %SPLIT%
+ALTER TABLE extractedsource ALTER COLUMN decl_sys_err SET NOT NULL;%SPLIT%
+

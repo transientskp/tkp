141e9ef8b5d19744b75558d45b32ec489932e30f
Gijs Molenaar <gijs@pythonic.nl>
2012-12-13 12:34:10 +0100

    rewrote for flexilbity and clarity

--- tkp/utility/accessors/lofarcasaimage.py
+++ tkp/utility/accessors/lofarcasaimage.py
@@ -1,5 +1,6 @@
 import logging
 import warnings
+import datetime
 import numpy
 from pyrap.tables import table as pyrap_table
 from tkp.utility.accessors.beam import degrees2pixels, arcsec2degrees
@@ -27,6 +28,149 @@ freq_units = {
     'GHz': 10**9,
 }
 
+
+def open_subtables(table):
+    """open all subtables defined in the LOFAR format
+    args:
+        table: a pyrap table handler to a LOFAR CASA table
+    returns:
+        a dict containing all LOFAR CASA subtables
+    """
+    subtables = {}
+    for subtable in subtable_names:
+        subtable_location = table.getkeyword("ATTRGROUPS")[subtable]
+        subtables[subtable] = pyrap_table(subtable_location, ack=False)
+    return subtables
+
+
+def parse_coordinates(table, wcs):
+    """extract coordination properties from headers
+    TODO: it would be better if this returns a new WCS object and not to modify
+          an the wcs arguments
+    """
+    my_coordinates = table.getkeyword('coords')['direction0']
+    wcs.crval = my_coordinates['crval']
+    wcs.crpix = my_coordinates['crpix']
+    wcs.cdelt = my_coordinates['cdelt']
+    ctype = ['unknown', 'unknown']
+    # What about other projections?!
+    if my_coordinates['projection'] == "SIN":
+        if my_coordinates['axes'][0] == "Right Ascension":
+            ctype[0] = "RA---SIN"
+        if my_coordinates['axes'][1] == "Declination":
+            ctype[1] = "DEC--SIN"
+    wcs.ctype = tuple(ctype)
+    # Rotation, units? We better set a default
+    wcs.crota = (0., 0.)
+    wcs.cunits = ('unknown', 'unknown')
+    # Update WCS
+    wcs.wcsset()
+
+
+def parse_frequency(table, observation_table):
+    """extract frequency related information from headers"""
+    freq_eff = table.getkeywords()['coords']['spectral2']['restfreq']
+    freq_bw = table.getkeywords()['coords']['spectral2']['wcs']['cdelt']
+    # subband
+    # see http://www.lofar.org/operations/doku.php?id=operator:background_to_observations&s[]=subband&s[]=width&s[]=clock&s[]=frequency
+    clockcol = observation_table.col('CLOCK_FREQUENCY')
+    clock = clockcol.getcol()[0]
+    unit = clockcol.getkeyword('QuantumUnits')[0]
+    trueclock = freq_units[unit] * clock
+    subbandwidth = trueclock / 1024
+    return (freq_eff, freq_bw, subbandwidth)
+
+
+def parse_data(table, plane=0):
+    """extract and massage data from CASA table"""
+    data = table[0]['map'].squeeze()
+    planes = len(data.shape)
+    if planes != 2:
+        msg = "received datacube with %s planes, assuming Stokes I and taking plane 0" % planes
+        logger.warn(msg)
+        warnings.warn(msg)
+        data = data[plane,:,:]
+    # TODO: is this required?
+    #data = data.transpose()
+    return data
+
+
+def parse_beam(beam, table, wcs):
+    """extract beam properties from headers"""
+    # TODO: this is used to be able to set beam parms manually, but this
+    # should be done in a different way
+    if beam:
+        bmaj, bmin, bpa = beam
+    else:
+        restoringbeam = table.getkeyword('imageinfo')['restoringbeam']
+        bmaj = restoringbeam['major']['value']
+        bmin = restoringbeam['minor']['value']
+        bpa = restoringbeam['positionangle']['value']
+
+    deltax = wcs.cdelt[0] * (180/pi)
+    deltay = wcs.cdelt[1] * (180/pi)
+
+    (degbmaj, degbmin, debbpa) = arcsec2degrees(bmaj, bmin, bpa)
+    return degrees2pixels(degbmaj, degbmin, debbpa, deltax, deltay)
+
+
+def parse_tautime(origin_table):
+    startcol = origin_table.col('START')
+    endcol = origin_table.col('END')
+    tau_time = endcol[0] - startcol[0]
+    return tau_time
+
+
+def parse_antennaset(observation_table):
+    return observation_table.getcol('ANTENNA_SET')[0]
+
+
+def parse_subbands(origin_table):
+   return origin_table.getcol('NUM_CHAN')[0]
+
+
+def parse_channels(origin_table):
+    return origin_table.getcol('NCHAN_AVG')[0]
+
+
+def parse_stations(observation_table, antenna_table):
+    """Extract number of specific LOFAR stations used
+    returns:
+        (number of core stations, remote stations, international stations)
+    """
+    nvis_used = observation_table.getcol('NVIS_USED')
+    station_ids = antenna_table.getcol('STATION_ID')
+    names = antenna_table.getcol('NAME')
+    mask = numpy.sum(nvis_used, axis=2) > 0
+    used_ids = station_ids[mask.ravel()]
+    used = [names[id] for id in used_ids]
+    ncore = 0
+    nremote = 0
+    nintl = 0
+    for station in used:
+        if station.startswith('CS'):
+            ncore += 1
+        elif station.startswith('RS'):
+            nremote += 1
+        else:
+            nintl += 1
+    return ncore, nremote, nintl
+
+
+def parse_phasecentre(table):
+    # TODO: this is not used anywhere but manually extracted in tkp-web!
+    phasecentre = table.getkeyword('coords')['pointingcenter']['value']
+    centre_ra, centre_decl = phasecentre
+    return float(centre_ra), float(centre_decl)
+
+def parse_taustartts(observation_table):
+    """ extract observation time from CASA table header
+    """
+    start = observation_table.getcol('OBSERVATION_START')[0]
+    taustart_ts = datetime.datetime.fromtimestamp(start)
+    return taustart_ts
+
+
 class LofarCasaImage(DataAccessor):
     """
     Use pyrap to pull image data out of an Casa table.
@@ -34,117 +178,27 @@ class LofarCasaImage(DataAccessor):
     This accessor assumes the casatable contains the values described in the
     CASA Image description for LOFAR. 0.03.00
     """
-    def __init__(self, filename, plane=0, beam=None):
+    def __init__(self, url, plane=0, beam=None):
         super(LofarCasaImage, self).__init__()  # Set defaults
-        self.filename = filename
-        self.table = pyrap_table(self.filename.encode(), ack=False)
+        self.url = url
+        self.table = pyrap_table(self.url.encode(), ack=False)
         self.beam = beam
-        self.subtables = {}
-        self.plane = plane
+        self.subtables = open_subtables(self.table)
+        parse_coordinates(self.table, self.wcs)
+        self.freq_eff, self.freq_bw, self.subbandwidth = parse_frequency(self.table,
+                                        self.subtables['LOFAR_OBSERVATION'])
+        self.beam = parse_beam(self.beam, self.table, self.wcs)
+        self.data = parse_data(self.table, plane)
+        self.tau_time = parse_tautime(self.subtables['LOFAR_ORIGIN'])
+        self.antenna_set = parse_antennaset(self.subtables['LOFAR_OBSERVATION'])
+        self.subbands = parse_subbands(self.subtables['LOFAR_ORIGIN'])
+        self.channels = parse_channels(self.subtables['LOFAR_ORIGIN'])
+        self.ncore, self.nremote, self.nintl = parse_stations(self.subtables['LOFAR_OBSERVATION'],
+                                            self.subtables['LOFAR_ANTENNA'])
+        self.taustart_ts = parse_taustartts(self.subtables['LOFAR_OBSERVATION'])
 
-        self._subtableparse()
-        self._coordparse()
-        self._freqparse()
-        self._beamsizeparse()
-        self._read_data()
-        self._otherparse()
-
-
-    def _read_data(self):
-        """extract and massage data from table"""
-        self.data = self.table[0]['map'].squeeze()
-        if len(self.data.shape) != 2:
-            logger.warn("received datacube with %s planes, assuming Stokes I and taking plane 0" % planes)
-            self.data = self.data[0,:,:]
-
-            # TODO: is this required?
-            #self.data = data.transpose()
-
-    def _freqparse(self):
-        """extract frequency related information from headers"""
-        self.freqeff = self.table.getkeywords()['coords']['spectral2']['restfreq']
-        self.freqbw = self.table.getkeywords()['coords']['spectral2']['wcs']['cdelt']
-
-        # subband
-        # see http://www.lofar.org/operations/doku.php?id=operator:background_to_observations&s[]=subband&s[]=width&s[]=clock&s[]=frequency
-        clockcol = self.subtables['LOFAR_OBSERVATION'].col('CLOCK_FREQUENCY')
-        clock = clockcol.getcol()[0]
-        unit = clockcol.getkeyword('QuantumUnits')[0]
-        trueclock = freq_units[unit] * clock
-        self.subbandwidth = trueclock / 1024
+        # TODO: this is not used anywhere but manually extracted in tkp-web!
+        self.centre_ra, self.centre_decl = parse_phasecentre(self.table)
 
 
-    def _coordparse(self):
-        """extract coordination properties from headers"""
-        my_coordinates = self.table.getkeyword('coords')['direction0']
-        self.wcs.crval = my_coordinates['crval']
-        self.wcs.crpix = my_coordinates['crpix']
-        self.wcs.cdelt = my_coordinates['cdelt']
-        ctype = ['unknown', 'unknown']
-        # What about other projections?!
-        if my_coordinates['projection'] == "SIN":
-            if my_coordinates['axes'][0] == "Right Ascension":
-                ctype[0] = "RA---SIN"
-            if my_coordinates['axes'][1] == "Declination":
-                ctype[1] = "DEC--SIN"
-        self.wcs.ctype = tuple(ctype)
-        # Rotation, units? We better set a default
-        self.wcs.crota = (0., 0.)
-        self.wcs.cunits = ('unknown', 'unknown')
-        # Update WCS
-        self.wcs.wcsset()
-        self.pix_to_position = self.wcs.p2s
-
-    def _beamsizeparse(self):
-        """extract beam properties from headers"""
-        if self.beam:
-            bmaj, bmin, bpa = self.beam
-        else:
-            restoringbeam = self.table.getkeyword('imageinfo')['restoringbeam']
-            bmaj = restoringbeam['major']['value']
-            bmin = restoringbeam['minor']['value']
-            bpa = restoringbeam['positionangle']['value']
-
-        deltax = self.wcs.cdelt[0] * (180/pi)
-        deltay = self.wcs.cdelt[1] * (180/pi)
-
-        (degbmaj, degbmin, debbpa) = arcsec2degrees(bmaj, bmin, bpa)
-        self.beam = degrees2pixels(degbmaj, degbmin, debbpa, deltax, deltay)
-
-    def _subtableparse(self):
-        """open all subtables"""
-        if not 'ATTRGROUPS' in self.table.getkeywords():
-            msg = "LOFAR standard violation: ATTRGROUPS not defined in %s" % self.filename
-            logger.warn(msg)
-            warnings.warn(msg)
-            return
-
-        for subtable in subtable_names:
-            subtable_location = self.table.getkeyword("ATTRGROUPS")[subtable]
-            self.subtables[subtable] = pyrap_table(subtable_location, ack=False)
-
-
-    def _otherparse(self):
-        startcol = self.subtables['LOFAR_ORIGIN'].col('START')
-        endcol = self.subtables['LOFAR_ORIGIN'].col('END')
-        self.intgr_time = endcol[0] - startcol[0]
-        self.antenna_set = self.subtables['LOFAR_OBSERVATION'].getcol('ANTENNA_SET')[0]
-        self.subbands = self.subtables['LOFAR_ORIGIN'].getcol('NUM_CHAN')[0]
-        self.channels = self.subtables['LOFAR_ORIGIN'].getcol('NCHAN_AVG')[0]
-        nvis_used = self.subtables['LOFAR_OBSERVATION'].getcol('NVIS_USED')
-        station_ids = self.subtables['LOFAR_ANTENNA'].getcol('STATION_ID')
-        names = self.subtables['LOFAR_ANTENNA'].getcol('NAME')
-        mask = numpy.sum(nvis_used, axis=2) > 0
-        used_ids = station_ids[mask.ravel()]
-        used = [names[id] for id in used_ids]
-        self.ncore = 0
-        self.nremote = 0
-        self.nintl = 0
-        for station in used:
-            if station.startswith('CS'):
-                self.ncore += 1
-            elif station.startswith('RS'):
-                self.nremote += 1
-            else:
-                self.nintl += 1
 

f258d322ad60acee7ef8f847b27081405604ef3f
Gijs Molenaar <gijs@pythonic.nl>
2013-01-15 13:48:18 +0100

    remove all default values

--- tkp/utility/accessors/dataaccessor.py
+++ tkp/utility/accessors/dataaccessor.py
@@ -8,9 +8,8 @@ def extract_metadata(dataaccessor):
         'tau_time': dataaccessor.tau_time,
         'freq_eff': dataaccessor.freq_eff,
         'freq_bw': dataaccessor.freq_bw,
-        'taustart_ts': dataaccessor.taustart_ts.strftime(time_format),
+        'taustart_ts': dataaccessor.taustart_ts,
         'url': dataaccessor.url,
-        'band': 0,    # not yet clearly defined
         'bsmaj': float(dataaccessor.beam[0]), ## NB We must cast to a standard python float
         'bsmin': float(dataaccessor.beam[1]), ## as Monetdb converter cannot handle numpy.float64
         'bpa': float(dataaccessor.beam[2]),
@@ -36,21 +35,22 @@ class DataAccessor(object):
     """
 
     def __init__(self):
-        self.beam = (None, None, None) # bmaj (px), bmin (px), bpa (rad)
         self.wcs = WCS()
-        self.tau_time = 0.  # integration seconds
-        self.taustart_ts = datetime.datetime(1970, 1, 1, 0, 0, 0)
-        self.freq_bw = 0.
-        self.freq_eff = 0.  # Hertz (? MHz?)
-        self.subbandwidth = 0.
-        self.url = "not set"
         self.data = None
-        self.centre_ra = 0
-        self.centre_decl = 0
+
+        self.beam = None # (bmaj (px), bmin (px), bpa (rad))
+        self.tau_time = None  # integration seconds
+        self.taustart_ts = None
+        self.freq_bw = None
+        self.freq_eff = None  # Hertz (? MHz?)
+        self.subbandwidth = None
+        self.url = None
+        self.centre_ra = None
+        self.centre_decl = None
         self.antenna_set = None
-        self.subbands = 0
-        self.channels = 0
-        self.ncore = 0
-        self.nremote = 0
-        self.nintl = 0
+        self.subbands = None
+        self.channels = None
+        self.ncore = None
+        self.nremote = None
+        self.nintl = None
         self.position = None

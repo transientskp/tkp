1679401e6a9a3563242ba03e522a0bb27349331b
Gijs Molenaar <gijs@pythonic.nl>
2013-02-18 14:08:15 +0100

    remove dead scripts

--- dead_scripts/editsettings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-file="/home/scheers/tkp-code/pipe/python-packages/tkp_lib/edset.py"
-if [ -e $file ]; then
-	echo "File exists"
-	sed 's/database_enabled = False/database_enabled = True/g' /home/scheers/tkp-code/pipe/python-packages/tkp_lib/edset.py > /home/scheers/tkp-code/pipe/python-packages/tkp_lib/edset.py.new
-	cp /home/scheers/tkp-code/pipe/python-packages/tkp_lib/edset.py.new /home/scheers/tkp-code/pipe/python-packages/tkp_lib/edset.py
-else
-	echo "File exists"
-fi

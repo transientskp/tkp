6aceb7c349d6661c254d8b60e88f7f1dcac976f8
Gijs Molenaar <gijs@pythonic.nl>
2013-02-21 18:09:11 +0100

    enable all options!

--- tests/test_tkp/test_bin/test_pyse.py
+++ tests/test_tkp/test_bin/test_pyse.py
@@ -1,18 +1,21 @@
 import os
 import unittest
-from tempfile import NamedTemporaryFile
+import tempfile
+import shutil
 import tkp.bin.pyse
 import tkp.config
 from tkp.utility.accessors import FitsImage
 from tkp.utility.accessors import sourcefinder_image_from_accessor
 
+
 data_path = tkp.config.config['test']['datapath']
-fits_file = os.path.join(data_path, 'L15_12h_const/observed-all.fits')
+orig_fits_file = os.path.join(data_path, 'L15_12h_const/observed-all.fits')
 
 
 class AttributeDict(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
+
 
 options = AttributeDict({
     'grid': 100,
@@ -20,25 +23,30 @@ options = AttributeDict({
     'radius': 0,
     'deblend': False,
     'deblend_thresholds': 32,
-    'residuals': False,
-    'islands': False,
-    'fdr': False,
+    'residuals': True,
+    'islands': True,
+    'fdr': True,
     'bmaj': 0.5,
     'bmin': 0.5,
     'bpa': 0.5,
     'detection': 5,
     'analysis': 5,
-    'regions': False,
-    'rmsmap': False,
-    'sigmap': False,
-    'skymodel': False,
-    'csv': False,
+    'regions': True,
+    'rmsmap': True,
+    'sigmap': True,
+    'skymodel': True,
+    'csv': True,
+    'alpha': .1,
 })
+
 
 class TestPyse(unittest.TestCase):
     def __init__(self, *args):
         super(TestPyse, self).__init__(*args)
-        self.fits = FitsImage(fits_file)
+        temp_dir = tempfile.mkdtemp()
+        self.filename = os.path.join(temp_dir, 'playground.fits')
+        shutil.copy(orig_fits_file, self.filename)
+        self.fits = FitsImage(self.filename)
         self.fits.beam = (.5, .5, .5)
         self.imagedata = sourcefinder_image_from_accessor(self.fits)
         self.sourcelist = self.imagedata.extract()
@@ -53,19 +61,19 @@ class TestPyse(unittest.TestCase):
         tkp.bin.pyse.csv(self.sourcelist)
 
     def test_summary(self):
-        tkp.bin.pyse.summary(fits_file, self.sourcelist)
+        tkp.bin.pyse.summary(self.filename, self.sourcelist)
 
     def test_handle_args(self):
         tkp.bin.pyse.handle_args()
 
     def test_writefites(self):
-        tempfile = NamedTemporaryFile(delete=False)
-        filename = tempfile.name
+        t = tempfile.NamedTemporaryFile(delete=False)
+        f = t.name
         data = self.fits.data
-        tkp.bin.pyse.writefits(filename, data, header={})
+        tkp.bin.pyse.writefits(f, data, header={})
 
     def test_run_sourcefinder(self):
         # no options and files
         tkp.bin.pyse.run_sourcefinder([], options)
         # one file
-        tkp.bin.pyse.run_sourcefinder([fits_file], options)
+        tkp.bin.pyse.run_sourcefinder([self.filename], options)

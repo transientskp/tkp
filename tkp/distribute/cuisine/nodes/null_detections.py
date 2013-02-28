528cce5a3a655de9f480d9ad129f8dc1e94bddb1
Gijs Molenaar <gijs@pythonic.nl>
2013-02-28 15:09:24 +0100

    missed this during merge

--- tkp/distribute/cuisine/nodes/null_detections.py
+++ tkp/distribute/cuisine/nodes/null_detections.py
@@ -3,8 +3,8 @@ from tkp.distribute.cuisine.common import node_run, TrapNode
 
 
 class null_detections(TrapNode):
-    def trapstep(self, image_id, image_path, image_nd):
-        self.outputs['ff_nd'] = steps.source_extraction.forced_fits(image_path, image_nd)
+    def trapstep(self, image_id, image_path, image_nd, parset_file):
+        self.outputs['ff_nd'] = steps.source_extraction.forced_fits(image_path, image_nd, parset_file)
         self.outputs['image_id'] = image_id
 
 node_run(__name__, null_detections)

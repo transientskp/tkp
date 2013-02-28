3972c4a16894732db418a2d04f36b5104e0fac86
Gijs Molenaar <gijs@pythonic.nl>
2013-02-28 17:02:42 +0100

    add rms code in own namespace

---
+++ tkp/quality/rms.py
@@ -0,0 +1,21 @@
+from tkp.utility import nice_format
+
+
+def rms_invalid(rms, noise, low_bound=1, high_bound=50):
+    """
+    Is the RMS value of an image too high?
+
+    :param rms: RMS value of an image, can be computed with
+                tkp.quality.statistics.rms
+    :param noise: Theoretical noise level of instrument, can be calculated with
+                  tkp.lofar.noise.noise_level
+    :param low_bound: multiplied with noise to define lower threshold
+    :param high_bound: multiplied with noise to define upper threshold
+    :returns: True/False
+    """
+    if (rms < noise * low_bound) or (rms > noise * high_bound):
+        ratio = rms / noise
+        return "rms value (%s) is %s times theoretical noise (%s)" % \
+                    (nice_format(rms), nice_format(ratio), nice_format(noise))
+    else:
+        return False

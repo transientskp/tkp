23184ca1b32b35c186d6bead1bdb112b7b5fa019
Tim Staley <tim.staley@soton.ac.uk>
2013-05-21 17:32:16 +0100

    Add 'runtests.sh' script, tests against both engines.

---
+++ tests/runtests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export TKP_TESTPATH=/opt/share/repos/tkp-data/unittests/tkp_lib 
+export TKP_DBNAME=testdb
+
+echo "****************************"
+echo "MONETDB:"
+echo "****************************"
+export TKP_DBENGINE=monetdb
+python ./runtests.py $*
+
+
+echo "****************************"
+echo "POSTGRES:"
+echo "****************************"
+export TKP_DBENGINE=postgresql
+export TKP_DBUSER=postgres
+python ./runtests.py $*
+

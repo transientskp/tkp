68c81c74e6e657b671e9d2e52c3726d082185f41
Gijs Molenaar <gijs@pythonic.nl>
2013-06-14 14:11:24 +0200

    add note about EAGER mode

--- tkp/conf/project_template/celeryconfig.py
+++ tkp/conf/project_template/celeryconfig.py
@@ -5,3 +5,7 @@ BROKER_URL = "redis://localhost:6379/0"
 CELERY_IMPORTS = ("tkp.distribute.celery.tasks", )
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 CELERYD_HIJACK_ROOT_LOGGER = False
+
+
+# TODO: This is supposed to make celery run local, but I don't get it to work
+# CELERY_ALWAYS_EAGER = CELERY_EAGER_PROPAGATES_EXCEPTIONS = True

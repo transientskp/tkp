275b7423bc13a95f18bb63ccc4b7560c9ddb9244
Tim Staley <tim.staley@soton.ac.uk>
2013-06-19 16:01:39 +0100

    Ensure port number specified in database section of parset is loaded as int.
    Also, add passphrase.
    
    Meanwhile, add docstrings to explain parser magic.

--- tkp/utility/parset.py
+++ tkp/utility/parset.py
@@ -1,3 +1,6 @@
+"""Utilities for loading parameters from config files, with automatic type 
+conversion."""
+
 import ConfigParser
 import ast
 import logging
@@ -18,10 +21,27 @@ def dumps_timestamp_w_microseconds(dt):
 
 #NB default dumps_method is 'repr' (implemented in dump_section)
 dumps_methods = { datetime.datetime : dumps_timestamp_w_microseconds}
-loads_methods = [ ast.literal_eval,
-                  loads_timestamp_w_microseconds]
+loads_methods = (ast.literal_eval,
+                  loads_timestamp_w_microseconds)
 
 def load_section(config, section):
+    """Loads the specified section of a parset file as a dictionary.
+    
+    Automatically converts strings representing ints and floats to their
+    respective types, through the magic of ast.literal_eval.
+    This functionality is extensible via the loads_methods list.
+    Each loads (load string) method is tested in turn to see if it 
+    throws an exception. If all throw, we assume the value is meant to be
+    string. 
+
+    Any values that you don't want to be converted should simply be 
+    surrounded with quote marks in the parset - then ast.literal_eval
+    knows to load it as a string. 
+
+    Note that as a feature of ConfigParser, and variable defined in 
+    the section 'DEFAULT' are present in all other sections, 
+    so you might get more than you expected.
+    """
     pars = {}
     for k, rawval in config.items(section):
         val = rawval
@@ -32,7 +52,7 @@ def load_section(config, section):
             except (ValueError, SyntaxError):
                 pass #Try the next method
         if val == rawval:
-            logger.warn("Parsing section: [%s]\n"
+            logger.debug("Parsing section: [%s]\n"
                         "Could not parse key-value pair:\n%s = %s\n"
                                 "-assuming string value",
                                 section, k, rawval)
@@ -41,6 +61,17 @@ def load_section(config, section):
     return pars
 
 def dump_section(config, section, params):
+    """
+    Writes a dictionary (params) into a ConfigParser object, 
+    under the specified section header.
+    
+    Normally ConfigParser only accepts string values, but here
+    type conversion to string is handled automatically via the dumps_methods
+    (dump string methods). This uses the type of a variable to check if 
+    we have defined a specific output method. Otherwise, we try the 
+    standard 'repr' function.
+    
+    """
     if not section in config.sections():
         config.add_section(section)
     for k, v in params.iteritems():

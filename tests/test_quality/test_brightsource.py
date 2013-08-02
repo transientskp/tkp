6af188d1d3d1622cf605720894a538d7f70eba63
John Swinbank <swinbank@transientskp.org>
2013-08-02 14:55:22 +0200

    Add a test for outdated ephemeris
    Addressing #3976.

--- tests/test_quality/test_brightsource.py
+++ tests/test_quality/test_brightsource.py
@@ -1,5 +1,6 @@
 import os
 from math import degrees
+from pyrap.measures import measures
 
 import unittest2 as unittest
 
@@ -8,9 +9,21 @@ import tkp.quality.brightsource
 import tkp.accessors
 from tkp.testutil.data import DATAPATH
 
-
 testimage = os.path.join(DATAPATH,
     'casatable/L55614_020TO029_skymodellsc_wmax6000_noise_mult10_cell40_npix512_wplanes215.img.restored.corr')
+
+class TestEphemeris(unittest.TestCase):
+    # Tests whether we can correctly identify that an ephemeris is invalid.
+    # I don't think it's possible to test that we can correctly identify a
+    # valid ephemeris, because we can't be sure that the user actually has one
+    # on disk (unless we ship one with the test case, but I don't think it's
+    # worth it.)
+    def test_invalid_ephemeris(self):
+        dm = measures()
+        # No(?) ephemeris we're likely to come across is valid at MJD 0.
+        dm.do_frame(dm.epoch("UTC", "0.0d"))
+        self.assertFalse(tkp.quality.brightsource.check_for_valid_ephemeris(dm))
+
 
 class TestBrightsource(unittest.TestCase):
     @requires_data(testimage)
---
+++ tkp/utility/redirect_stream.py
@@ -0,0 +1,19 @@
+import os
+from tempfile import SpooledTemporaryFile
+from contextlib import contextmanager
+
+@contextmanager
+def redirect_stream(output_stream, destination):
+    """
+    Redirect anything written to ``output_stream`` (typically ``sys.stdin`` or
+    ``sys.stdout``) to ``destination`` for the duration of this context.
+
+    ``destination`` must provide a ``write()`` method.
+    """
+    old_stream = os.dup(output_stream.fileno())
+    with SpooledTemporaryFile() as s:
+        os.dup2(s.fileno(), output_stream.fileno())
+        yield
+        s.seek(0)
+        destination.write(s.read())
+    os.dup2(old_stream, output_stream.fileno())

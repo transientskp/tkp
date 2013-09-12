202422430936c38c24dc1bd2b4f0df435ae446ad
John Swinbank <j.swinbank@uva.nl>
2013-09-12 10:13:55 +0200

    Use "image" coordinates, as per #4770

--- tkp/bin/pyse.py
+++ tkp/bin/pyse.py
@@ -41,7 +41,7 @@ def regions(sourcelist):
     output = StringIO()
     print >>output, "# Region file format: DS9 version 4.1"
     print >>output, "global color=green dashlist=8 3 width=1 font=\"helvetica 10 normal\" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1"
-    print >> output, "physical"
+    print >>output, "image"
     for source in sourcelist:
         # NB, here we convert from internal 0-origin indexing to DS9 1-origin indexing
         print >>output, "ellipse(%f, %f, %f, %f, %f)" % (

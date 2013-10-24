e7a3203ce8c79451c9e1c867beac8346e71d8fb3
John Swinbank <j.swinbank@uva.nl>
2013-10-24 15:06:28 +0200

    Clean up test code

--- tests/test_database/test_nulldetections.py
+++ tests/test_database/test_nulldetections.py
@@ -1,6 +1,5 @@
-import math
 import datetime
-import logging
+import itertools
 
 import unittest2 as unittest
 
@@ -27,66 +26,71 @@ class TestForcedFit(unittest.TestCase):
 
     def test_nullDetection(self):
         dataset = DataSet(data={'description': "null detection:" + self._testMethodName})
-        # We have three timestamps of an image cube with 4 frfequency bands
-        n_images = 12
-        im_params = db_subs.example_dbimage_datasets(n_images)
-        # Three timestamps
+
+        # Three timesteps, each with 4 bands -> 12 images.
         taustart_tss = [datetime.datetime(2013, 8, 1),
-                        datetime.datetime(2013, 9, 1), 
+                        datetime.datetime(2013, 9, 1),
                         datetime.datetime(2013, 10, 1)]
-        # Four frequencies/bands
         freq_effs = [124, 149, 156, 185]
-        freq_effs[:] = [f * 1000000.0 for f in freq_effs]
-        for idx, im in enumerate(im_params):
-            print idx, idx%len(freq_effs), (idx - (idx%len(freq_effs)))/len(freq_effs)
-            im['freq_eff'] = freq_effs[idx%len(freq_effs)]
-            im['taustart_ts'] = taustart_tss[(idx - (idx%len(freq_effs)))/len(freq_effs)]
+        freq_effs = [f * 1e6 for f in freq_effs]
+
+        im_params = db_subs.example_dbimage_datasets(len(freq_effs) * len(taustart_tss))
+        timestamps = itertools.repeat(taustart_tss, len(freq_effs))
+
+        for im, freq, ts in zip(im_params, itertools.cycle(freq_effs),
+            itertools.chain.from_iterable(zip(*timestamps))
+        ):
+            im['freq_eff'] = freq
+            im['taustart_ts'] = ts
 
         images = []
         for im in im_params:
-            print im
             image = tkp.db.Image(dataset=dataset, data=im)
             images.append(image)
 
         # Source properties are the same in every image
         src = db_subs.example_extractedsource_tuple()
-        
-        # group images by timestamp 
-        grouped_images = [images[0:4], images[4:8], images[8:12]]
-        for images in grouped_images:
+
+        # group images in blocks of 4, corresponding to all frequency bands at
+        # a given timestep.
+        for images in zip(*(iter(images),) * len(freq_effs)):
             for image in images:
+                # We 'skip' a couple of images to simulate missed blind
+                # detections.
                 print image.id, image.taustart_ts, image.freq_eff, type(image.taustart_ts)
-                if ((image.taustart_ts == taustart_tss[1] and image.freq_eff == freq_effs[0])
-                or (image.taustart_ts == taustart_tss[2] and image.freq_eff == freq_effs[2])): 
+                if (
+                    (image.taustart_ts == taustart_tss[1] and image.freq_eff == freq_effs[0]) or
+                    (image.taustart_ts == taustart_tss[2] and image.freq_eff == freq_effs[2])
+                ):
                     pass
                 else:
                     dbgen.insert_extracted_sources(image.id, [src], 'blind')
-            
+
             for image in images:
-                null_detections = dbmon.get_nulldetections(image.id, 5.68) 
-                print null_detections
-                # image t2_b2 does not have detection, so there will be forced fit at 
-                # the null_detection position
-                if ((image.taustart_ts == taustart_tss[1] and image.freq_eff == freq_effs[0])
-                or (image.taustart_ts == taustart_tss[2] and image.freq_eff == freq_effs[2])): 
+                # The images we skipped above will have null detections at the
+                # source position; the others should have no null detections.
+                null_detections = dbmon.get_nulldetections(image.id, 5.68)
+                if (
+                    (image.taustart_ts == taustart_tss[1] and image.freq_eff == freq_effs[0]) or
+                    (image.taustart_ts == taustart_tss[2] and image.freq_eff == freq_effs[2])
+                ):
                     self.assertEqual(null_detections[0][0], src.ra)
                     self.assertEqual(null_detections[0][1], src.dec)
                     dbgen.insert_extracted_sources(image.id, [src], 'ff_nd')
+                else:
+                    self.assertEqual(len(null_detections), 0)
                 dbass.associate_extracted_sources(image.id, deRuiter_r=5.68)
 
         query = """\
         SELECT id
               ,datapoints
-          FROM runningcatalog r 
-         WHERE dataset = %(dataset_id)s
+        FROM runningcatalog r
+        WHERE dataset = %(dataset_id)s
         """
         cursor = tkp.db.execute(query, {'dataset_id': dataset.id})
-        result = zip(*cursor.fetchall())
-        self.assertNotEqual(len(result), 0)
-        runcat = result[0]
-        datapoints = result[1]
+        result = cursor.fetchall()
+        # We should have a single running catalog source
+        self.assertEqual(len(result), 1)
 
-        self.assertEqual(len(runcat), 1)
-        self.assertEqual(datapoints[0], n_images)
-
-        #self.assertEqual(1,2)
+        # With a datapoint for every image
+        self.assertEqual(result[0][1], len(im_params))

c964e5c38a2a2984460c83f86a69c368d0abc520
Gijs Molenaar <gijs@pythonic.nl>
2014-06-10 12:43:39 +0200

    fix multiproc

--- tkp/distribute/multiproc/__init__.py
+++ tkp/distribute/multiproc/__init__.py
@@ -1,8 +1,14 @@
+"""
+A computation distribution implementation using the build in multiprocessing
+module. the Pool.map function only accepts one argument, so we need to
+zip the iterable together with the arguments.
+"""
 from multiprocessing import Pool, cpu_count
 
 pool = Pool(processes=cpu_count())
 
 
 def map(func, iterable, args):
-    partial = lambda i: func(i, *args)
-    pool.map(partial, iterable)
+    zipped = ((i, args) for i in iterable)
+    return pool.map(func, zipped)
+
--- tkp/distribute/multiproc/tasks.py
+++ tkp/distribute/multiproc/tasks.py
@@ -9,29 +9,37 @@ import tkp.steps
 logger = logging.getLogger(__name__)
 
 
-def persistence_node_step(images, image_cache_config):
+def persistence_node_step(zipped):
     logger.info("running persistence task")
+    images, args = zipped
+    image_cache_config = args[0]
     return tkp.steps.persistence.node_steps(images, image_cache_config)
 
 
-def quality_reject_check(url, job_config):
+def quality_reject_check(zipped):
     logger.info("running quality task")
+    url, args = zipped
+    job_config = args[0]
     return tkp.steps.quality.reject_check(url, job_config)
 
 
-def extract_sources(url, extraction_params):
+def extract_sources(zipped):
     logger.info("running extracted sources task")
+    url, args = zipped
+    extraction_params = args[0]
     return tkp.steps.source_extraction.extract_sources(url, extraction_params)
 
 
-def forced_fits(detection_set, extraction_params):
+def forced_fits(zipped):
     """
     :param detection_set: should be (url, detections) tuple
     :param extraction_params: null detections extraction_params
     :return:
     """
     logger.info("running forced fits task")
+    detection_set, args = zipped
     url, detections = detection_set
+    extraction_params = args[0]
     return tkp.steps.source_extraction.forced_fits(url, detections,
                                                    extraction_params)
 

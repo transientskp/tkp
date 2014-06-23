35417224788a1b6fb4e53d41d11e7a55a36b080e
John Swinbank <j.swinbank@uva.nl>
2014-06-23 11:09:11 +0200

    Minor editing of docs

--- documentation/devref/database/schema/settings.py
+++ documentation/devref/database/schema/settings.py
@@ -3,12 +3,13 @@ SECRET_KEY = 'bla'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gijs',
-        'USER': 'gijs',
-        'PASSWORD': 'gijs',
+        'NAME': 'XXX',
+        'USER': 'XXX',
+        'PASSWORD': 'XXX',
+        'HOST': 'XXX',
+        'PORT': XXX
     }
 }
-
 
 INSTALLED_APPS = (
     'django_extensions',

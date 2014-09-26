3d72feeaad9a98b7cfa88d465aa4883f34da8ad0
Tim Staley <tim.staley@astro.ox.ac.uk>
2014-09-26 11:54:05 +0100

    Revert "Revert "Merge pull request #337 from timstaley/median-rebased""
    This reverts commit da0c681e6d0718b2a48b3c8b50a022c7944520f5.

---
+++ tkp/db/sql/statements/functions/median.sql
@@ -0,0 +1,36 @@
+{% ifdb postgresql %}
+
+/*
+  Defines 'median' aggregate function, cf.
+  https://wiki.postgresql.org/wiki/Aggregate_Median
+*/
+
+CREATE FUNCTION _final_median(anyarray) RETURNS float8 AS $$
+  WITH q AS
+  (
+     SELECT val
+     FROM unnest($1) val
+     WHERE VAL IS NOT NULL
+     ORDER BY 1
+  ),
+  cnt AS
+  (
+    SELECT COUNT(*) AS c FROM q
+  )
+  SELECT AVG(val)::float8
+  FROM
+  (
+    SELECT val FROM q
+    LIMIT  2 - MOD((SELECT c FROM cnt), 2)
+    OFFSET GREATEST(CEIL((SELECT c FROM cnt) / 2.0) - 1,0)
+  ) q2;
+$$ LANGUAGE sql IMMUTABLE;
+
+CREATE AGGREGATE median(anyelement) (
+  SFUNC=array_append,
+  STYPE=anyarray,
+  FINALFUNC=_final_median,
+  INITCOND='{}'
+);
+
+{% endifdb %}

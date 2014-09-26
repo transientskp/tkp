da0c681e6d0718b2a48b3c8b50a022c7944520f5
Tim Staley <tim.staley@astro.ox.ac.uk>
2014-09-26 11:44:38 +0100

    Revert "Merge pull request #337 from timstaley/median-rebased"
    This reverts commit 41b7169ce1df5dd2fb3a75dda142a50cc190c799, reversing
    changes made to 83b095733f8c1280311681bd177cc0d4f64b81b8.
    
    Pending getting a fixed version cf.
    https://github.com/timstaley/tkp/tree/fix-median

--- tkp/db/sql/populate.py
+++ tkp/db/sql/populate.py
@@ -99,8 +99,8 @@ select 'drop table if exists "' || tablename || '" cascade;'
  where schemaname = 'public';
 """
     postgres_gen_drop_functions = """
-select 'drop function if exists ' || ns.nspname || '.' || proname
-       || '(' || oidvectortypes(proargtypes) || ') cascade;'
+select 'drop function ' || ns.nspname || '.' || proname
+       || '(' || oidvectortypes(proargtypes) || ');'
 from pg_proc inner join pg_namespace ns on (pg_proc.pronamespace = ns.oid)
 where ns.nspname = 'public'  order by proname;
 """

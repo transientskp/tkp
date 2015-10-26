d826c35f7da3cdc2d9f10a4d3cdc622e7414e013
Gijs Molenaar <gijs@pythonic.nl>
2015-10-26 11:49:42 +1100

    add missing rejectreason to database

--- tkp/db/sql/statements/init/rejectreasons.sql
+++ tkp/db/sql/statements/init/rejectreasons.sql
@@ -2,3 +2,4 @@ INSERT INTO rejectreason VALUES (0, 'RMS invalid');
 INSERT INTO rejectreason VALUES (1, 'beam invalid');
 INSERT INTO rejectreason VALUES (2, 'bright source near');
 INSERT INTO rejectreason VALUES (3, 'tau_time invalid');
+INSERT INTO rejectreason VALUES (4, 'Contains NaN value');

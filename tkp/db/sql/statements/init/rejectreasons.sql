f26e46cc2b84fd5e9e00ce65a99118c28f1d87d5
Gijs Molenaar <gijs@pythonic.nl>
2016-05-12 11:44:00 +0200

    add reject row for flat image

--- tkp/db/sql/statements/init/rejectreasons.sql
+++ tkp/db/sql/statements/init/rejectreasons.sql
@@ -3,3 +3,4 @@ INSERT INTO rejectreason VALUES (1, 'beam invalid');
 INSERT INTO rejectreason VALUES (2, 'bright source near');
 INSERT INTO rejectreason VALUES (3, 'tau_time invalid');
 INSERT INTO rejectreason VALUES (4, 'Contains NaN value');
+INSERT INTO rejectreason VALUES (5, 'data is flat');

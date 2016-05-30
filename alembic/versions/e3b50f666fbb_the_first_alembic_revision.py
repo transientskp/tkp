e92f9fe79507b4440bb5681205c4644ebe2ea117
Gijs Molenaar <gijs@pythonic.nl>
2016-05-30 11:47:49 +0200

    add alembic working files

---
+++ alembic/versions/e3b50f666fbb_the_first_alembic_revision.py
@@ -0,0 +1,24 @@
+"""the first alembic revision
+
+Revision ID: e3b50f666fbb
+Revises: 
+Create Date: 2016-05-30 11:32:22.585690
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e3b50f666fbb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ install_requires = """
     pywcs>=1.12
     scipy>=0.7.0
     sqlalchemy>=1.0.0
+    alembic
     """.split()
 
 extras_require = {

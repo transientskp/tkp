acc35b217906a96d085458043f1b4f159973b5dc
Bart Scheers <bscheers@cwi.nl>
2018-05-01 12:22:28 +0200

    Remove temprunningcatalog constraints

--- tkp/db/sql/statements/keys/drop_constraints.sql
+++ tkp/db/sql/statements/keys/drop_constraints.sql
@@ -1,2 +1,9 @@
 ALTER TABLE "trap"."assocxtrsource" DROP CONSTRAINT "assocxtrsource_runcat_fkey";
 ALTER TABLE "trap"."assocxtrsource" DROP CONSTRAINT "assocxtrsource_xtrsrc_fkey";
+ALTER TABLE "trap"."temprunningcatalog" DROP CONSTRAINT "temprunningcatalog_band_fkey";
+ALTER TABLE "trap"."temprunningcatalog" DROP CONSTRAINT "temprunningcatalog_dataset_fkey";
+ALTER TABLE "trap"."temprunningcatalog" DROP CONSTRAINT "temprunningcatalog_runcat_fkey";
+ALTER TABLE "trap"."temprunningcatalog" DROP CONSTRAINT "temprunningcatalog_xtrsrc_fkey";
+
+
+

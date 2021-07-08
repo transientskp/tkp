fe7cf21588df25a92d2677245052f46eab262b9f
antoniar <antonia.rowlinson@gmail.com>
2021-07-08 13:21:19 +0200

    removal of monetdb from test_dump.py

--- tests/test_database/test_dump.py
+++ tests/test_database/test_dump.py
@@ -2,7 +2,7 @@ import os
 import unittest
 from tempfile import NamedTemporaryFile
 from tkp.config import get_database_config
-from tkp.db.dump import dump_db, dump_monetdb, dump_pg
+from tkp.db.dump import dump_db, dump_pg
 from tkp.testutil.decorators import requires_database
 
 class TestDump(unittest.TestCase):
@@ -24,21 +24,6 @@ class TestDump(unittest.TestCase):
             dumpfile.seek(-41, os.SEEK_END)
             self.assertEqual(dumpfile.read().strip(), "-- PostgreSQL database dump complete\n--")
 
-    @requires_database()
-    @unittest.skipUnless(get_database_config()['engine'] == "monetdb", "Monet disabled")
-    def test_database_dump_monet(self):
-        dbconfig = get_database_config()
-        with NamedTemporaryFile() as dumpfile:
-            dump_monetdb(
-                dbconfig['host'], dbconfig['port'], dbconfig['database'],
-                dbconfig['user'], dbconfig['password'], dumpfile.name
-            )
-            # Output should start with "START TRANSACTION;" and end with
-            # "COMMIT;"
-            dumpfile.seek(0)
-            self.assertEqual(dumpfile.readline().strip(), "START TRANSACTION;")
-            dumpfile.seek(-8, os.SEEK_END)
-            self.assertEqual(dumpfile.readline().strip(), "COMMIT;")
 
     def test_database_dump_unknown(self):
         self.assertRaises(NotImplementedError, dump_db,

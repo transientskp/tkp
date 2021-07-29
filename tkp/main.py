311eb3badc9e3b3fe993332fd9bbf13aeebd6289
antoniar <antonia.rowlinson@gmail.com>
2021-07-29 12:36:19 +0200

    introduction of new function to conduct the basic quality checks on all of the images

--- tkp/main.py
+++ tkp/main.py
@@ -219,6 +219,36 @@ def quality_check(db_images, accessors, job_config, runner):
         logger.warn(msg)
     return good_images
 
+def quality_check_all(db_images, accessors, job_config, runner):
+    """                                                                                                                     
+    returns:                                                                                                                
+        tuple: a list of db_image and accessor tuples                                                                       
+    """
+    logger.debug("performing basic quality check on all images")
+    arguments = [job_config]
+    rejecteds = runner.map("quality_reject_check", accessors, arguments)
+
+    db = tkp.db.Database()
+    history = job_config.persistence.rms_est_history
+    rms_max = job_config.persistence.rms_est_max
+    rms_min = job_config.persistence.rms_est_min
+    est_sigma = job_config.persistence.rms_est_sigma
+    good_images = []
+    for db_image, rejected, accessor in zip(db_images, rejecteds, accessors):
+        if not rejected:
+            rejected = reject_basic_rms(db_image.id, db.session,
+                                             history, est_sigma, rms_max, rms_min)
+
+        if rejected:
+            reason, comment = rejected
+            steps.quality.reject_image(db_image.id, reason, comment)
+        else:
+            good_images.append((db_image, accessor))
+
+    if not good_images:
+        msg = "No good images under these quality checking criteria"
+        logger.warn(msg)
+    return good_images
 
 def source_extraction(accessors, job_config, runner):
     logger.debug("performing source extraction")

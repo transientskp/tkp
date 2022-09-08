f22ba03abebbd29530ac70a5002ac2637dca2139
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2022-09-08 16:44:34 +0200

    Trying to fix "Can't instantiate abstract class ParamSet" but it did not work in this simple way.

--- tkp/sourcefinder/extract.py
+++ tkp/sourcefinder/extract.py
@@ -17,7 +17,7 @@ try:
     from UserDict import DictMixin
 except ImportError:
     from collections import UserDict
-    from collections import MutableMapping as DictMixin
+    from collections.abc import MutableMapping as DictMixin
 
 import numpy
 

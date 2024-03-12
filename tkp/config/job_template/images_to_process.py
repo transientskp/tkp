b74612b28b9bf383df42a5ee9e5b9a682f4b558a
Antonia Rowlinson <antonia@antonias-air.home>
2024-03-12 09:35:45 +0100

    changing default in images_to_process

--- tkp/config/job_template/images_to_process.py
+++ tkp/config/job_template/images_to_process.py
@@ -6,21 +6,21 @@
 # This should provide a module-scope iterable named "images" which provides a
 # full path to each image to be processed. For example:
 
-images = [
-    "/path/to/image1",
-    "/path/to/image2",
-]
+#images = [
+#    "/path/to/image1",
+#    "/path/to/image2",
+#]
 
 # Optionally, whatever standard tools are required may be used to generate the
 # list:
 #
-#  import os
-#  import glob
-#  images = sorted(
-#      glob.glob(
-#          os.path.expanduser("/home/example/data/*.fits")
-#      )
-#  )
+import os
+import glob
+images = sorted(
+    glob.glob(
+        os.path.expanduser("/home/example/data/*.fits")
+        )
+    )
 
 #Display the list of images to be processed whenever this file is imported:
 # (can be used for quick checking via an ipython import)
